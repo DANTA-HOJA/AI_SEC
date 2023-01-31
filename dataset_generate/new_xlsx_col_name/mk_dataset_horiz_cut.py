0a8091c1c3c1e0686296d9e8e910c035f0bd25dc
r10945018 <rime97410000@gmail.com>
2023-02-01 06:44:36 +0800

    [recreate] dataset_generate/new_xlsx_col_name/

--- dataset_generate/new_xlsx_col_name/mk_dataset_horiz_cut.py
+++ dataset_generate/new_xlsx_col_name/mk_dataset_horiz_cut.py
@@ -17,6 +17,7 @@ from collections import Counter
 
 import cv2
 
+from dataset_generate_functions import create_new_dir, gen_crop_img, drop_too_dark
 
 
 # *** Show images methods ***
@@ -25,101 +26,6 @@ import cv2
 # plt_show.img_rgb("fish", fish)
 # plt_show.img_gray("fish", fish)
 # plt_show.img_by_channel("fish", fish)
-
-
-
-def create_new_dir(path:str, end="\n"):
-    if not os.path.exists(path):
-        # if the demo_folder directory is not exist then create it.
-        os.makedirs(path)
-        print(f"path: '{path}' is created!{end}")
-
-
-
-def gen_crop_img(img, crop_size:int, shift_region:float):
-    
-    """ 
-    generate the crop images using 'crop_size' and 'shift_region'
-    
-    Args:
-        img : the image you want to generate its crop images
-        crop_size (int): output size/shape of an crop image
-        shift_region (float): if 'shift_region' is passed, calculate the shift region while creating each cropped image, e.g. shift_region=1/3, the overlap region of each cropped image is 2/3
-    """
-    
-    img_size = img.shape
-    
-    
-    # *** calculate relation between img & crop_size ***
-    
-    # if 'shift_region' is passed, calculate the shift region while creating each cropped image, e.g. shift_region=1/3, the overlap region of each cropped image is 2/3
-    DIV_PIECES = int(1/shift_region) # DIV_PIECES, abbr. of 'divide pieces'
-    # print(DIV_PIECES, "\n")
-    # input()
-    
-    # Height
-    quotient_h = int(img_size[0]/crop_size) # Height Quotient
-    remainder_h = img_size[0]%crop_size # Height Remainder
-    h_st_idx = remainder_h # image 只有上方有黑色
-    h_crop_idxs = [(h_st_idx + int((crop_size/DIV_PIECES)*i)) 
-                           for i in range(quotient_h*DIV_PIECES+1)]
-    # print(f"h_crop_idxs = {h_crop_idxs}", f"len = {len(h_crop_idxs)}", "\n")
-    # input()
-    
-    # Width
-    quotient_w = int(img_size[1]/crop_size) # Width Quotient
-    remainder_w = img_size[1]%crop_size # Width Remainder
-    w_st_idx = int(remainder_w/2) # image 左右都有黑色，平分
-    w_crop_idxs = [(w_st_idx + int((crop_size/DIV_PIECES)*i))
-                           for i in range(quotient_w*DIV_PIECES+1)]
-    # print(f"w_crop_idxs = {w_crop_idxs}", f"len = {len(w_crop_idxs)}", "\n")
-    # input()
-    
-    
-    # *** crop original image to small images ***
-    
-    # do crop 
-    crop_img_list = []
-    for i in range(len(h_crop_idxs)-DIV_PIECES):
-        for j in range(len(w_crop_idxs)-DIV_PIECES):
-            # print(h_crop_idxs[i], h_crop_idxs[i+DIV_PIECES], "\n", w_crop_idxs[j], w_crop_idxs[j+DIV_PIECES], "\n")
-            crop_img_list.append(img[h_crop_idxs[i]:h_crop_idxs[i+DIV_PIECES], w_crop_idxs[j]:w_crop_idxs[j+DIV_PIECES], :])
-    
-    
-    return crop_img_list
-
-
-
-def drop_too_dark(crop_img_list:List, intensity:int, drop_ratio:float):
-    
-    """ 
-    drop the image which too many dark pixels
-    
-    Args:
-        crop_img_list (List): a list contain 'gray images'
-        crop_img_list_color (List): a list contain 'color(RGB) images'
-        intensity (int): a threshold (image is grayscale) to define too dark or not 
-        drop_ratio (float): a threshold (pixel_too_dark / all_pixel) to decide the crop image keep or drop, if drop_ratio < 0.5, keeps the crop image.
-    """
-    
-    
-    # check if the height and width of passed two lists with the same size
-    img_size = crop_img_list[0].shape
-    
-    select_crop_img_list = []
-
-    
-    for i in range(len(crop_img_list)):
-        # change to grayscale
-        in_grayscale = cv2.cvtColor(crop_img_list[i], cv2.COLOR_BGR2GRAY)
-        # count pixels too dark
-        pixel_too_dark = np.sum( in_grayscale < intensity )
-        dark_ratio = pixel_too_dark/(crop_size*crop_size)
-        if dark_ratio < drop_ratio: # 有表皮資訊的
-            select_crop_img_list.append(crop_img_list[i])
-
-
-    return select_crop_img_list
 
 
 
