2929678443b152dd1bb1f50041241129f34f9377
r10945018 <rime97410000@gmail.com>
2023-03-03 09:19:22 +0800

    [mod varname, dict_key+value] RGB_preprocess

--- data_operate/stacked_palmskin_RGB/collect_RGB_results.py
+++ data_operate/stacked_palmskin_RGB/collect_RGB_results.py
@@ -13,7 +13,7 @@ from fileop import create_new_dir
 
 
 def get_fish_ID(path:str) -> int:
-    # rgb: .../20220610_CE001_palmskin_8dpf - Series001_fish_1_A_RGB/[modality].tif
+    # rgb: .../20220610_CE001_palmskin_8dpf - Series001_fish_1_A_RGB/[file_with_extension]
     if "MetaImage" in path: fish_dir = path.split(os.sep)[-3]
     else: fish_dir = path.split(os.sep)[-2]
     fish_dir_list = re.split(" |_|-", fish_dir)
@@ -22,7 +22,7 @@ def get_fish_ID(path:str) -> int:
 
 
 def get_fish_pos(path:str) -> int:
-    # rgb: .../20220610_CE001_palmskin_8dpf - Series001_fish_1_A_RGB/[modality].tif
+    # rgb: .../20220610_CE001_palmskin_8dpf - Series001_fish_1_A_RGB/[file_with_extension]
     if "MetaImage" in path: fish_dir = path.split(os.sep)[-3]
     else: fish_dir = path.split(os.sep)[-2]
     fish_dir_list = re.split(" |_|-", fish_dir)
@@ -41,43 +41,42 @@ def resave_RGB_result(original_path:str, output_dir:str, result_key:str):
 
 
 # input
-ap_data = r"C:\Users\confocal_microscope\Desktop\{PyIJ_OutTest}_RGB_preprocess" # r"C:\Users\confocal_microscope\Desktop\WorkingDir\(D2)_Image_AP\{Data}_Data\{20221209_UPDATE_82}_Academia_Sinica_i324"
+ap_data_root = r"C:\Users\confocal_microscope\Desktop\{PyIJ_OutTest}_RGB_preprocess" # r"C:\Users\confocal_microscope\Desktop\WorkingDir\(D2)_Image_AP\{Data}_Data\{20221209_UPDATE_82}_Academia_Sinica_i324"
 preprocess_method_desc = "ch4_min_proj, outer_rect"
-preprocess_root = os.path.join(ap_data, f"{{{preprocess_method_desc}}}_RGB_preprocess")
+preprocess_root = os.path.join(ap_data_root, f"{{{preprocess_method_desc}}}_RGB_preprocess")
 
 
-# modality
+# result
 result_map = {
     "B_processed":                "MetaImage/01_B_processed.tif",
-	"B_processed_Kuwahara15":     "MetaImage/02_B_processed_Kuwahara15.tif",    # TODO:  imagej_RGB_preprocess 將 preprocess var 加入 log 後移除 para 後綴
+	"B_processed_Kuwahara":       "MetaImage/02_B_processed_Kuwahara*.tif",
     "G_processed":                "MetaImage/03_G_processed.tif",
-    "G_processed_Kuwahara15":     "MetaImage/04_G_processed_Kuwahara15.tif",    # TODO:  imagej_RGB_preprocess 將 preprocess var 加入 log 後移除 para 後綴
+    "G_processed_Kuwahara":       "MetaImage/04_G_processed_Kuwahara*.tif",
     "R_processed":                "MetaImage/05_R_processed.tif",
-    "R_processed_Kuwahara15":     "MetaImage/06_R_processed_Kuwahara15.tif",    # TODO:  imagej_RGB_preprocess 將 preprocess var 加入 log 後移除 para 後綴
+    "R_processed_Kuwahara":       "MetaImage/06_R_processed_Kuwahara*.tif",
     "RGB":                        "MetaImage/07_composite_RGB.tif",
     "RGB_HE" :                    "08_composite_RGB_HE.tif", # CHECK_PT 
     "RGB_Kuwahara":               "MetaImage/09_composite_RGB_Kuwahara.tif",
 	"RGB_Kuwahara_HE" :           "10_composite_RGB_Kuwahara_HE.tif", # CHECK_PT 
-	"RGB_HE_mix" :                "11_composite_HE_mix.tif", # CHECK_PT 
-    "BF_Zproj_min":               "MetaImage/12_BF_Zproj_min.tif",              # TODO:  imagej_RGB_preprocess 將 preprocess var 加入 log 後移除 para 後綴
-    "BF_Zproj_min_HE":            "MetaImage/13_BF_Zproj_min_HE.tif",
-    "Threshold":                  "MetaImage/14_Threshold_0_10.tif",            # TODO:  imagej_RGB_preprocess 將 preprocess var 加入 log 後移除 para 後綴
+	"RGB_HE_mix" :                "11_composite_RGB_HE_mix.tif", # CHECK_PT 
+    "BF_Zproj":                   "MetaImage/12_BF_Zproj_*.tif",
+    "BF_Zproj_HE":                "MetaImage/13_BF_Zproj_*_HE.tif",
+    "Threshold":                  "MetaImage/14_Threshold_*_*.tif",
     "outer_rect":                 "MetaImage/15_outer_rect.tif",
     "inner_rect":                 "MetaImage/16_inner_rect.tif",
     "RGB_HE--MIX":                "MetaImage/17_composite_RGB_HE--MIX.tif",
     "RGB_Kuwahara_HE--MIX":       "MetaImage/18_composite_RGB_Kuwahara_HE--MIX.tif",
-    "RGB_HE_mix--MIX":            "MetaImage/19_composite_HE_mix--MIX.tif",
+    "RGB_HE_mix--MIX":            "MetaImage/19_composite_RGB_HE_mix--MIX.tif",
     "cropped_RGB_HE" :            "20_cropped_composite_RGB_HE.tif", # CHECK_PT 
 	"cropped_RGB_Kuwahara_HE" :   "21_cropped_composite_RGB_Kuwahara_HE.tif", # CHECK_PT 
-	"cropped_RGB_HE_mix" :        "22_cropped_composite_HE_mix.tif", # CHECK_PT 
+	"cropped_RGB_HE_mix" :        "22_cropped_composite_RGB_HE_mix.tif", # CHECK_PT 
     "RoiSet" :                    "MetaImage/RoiSet.zip",
 }
 result_key = "RGB_HE_mix"
 
 
 # output
-output_dir = r"C:\Users\confocal_microscope\Desktop\RGB_reCollection"
-output_dir = os.path.join(output_dir, f"{{{preprocess_method_desc}}}", result_key)
+output_dir = os.path.join(ap_data_root, f"{{{preprocess_method_desc}}}_RGB_reCollection", result_key)
 create_new_dir(output_dir)
 
 
@@ -89,13 +88,13 @@ path_list.sort(key=get_fish_ID)
 summary = {}
 summary["result_key"] = result_key
 summary["actual_name"] = result_map[result_key]
-summary["max_probable_file"] = get_fish_ID(path_list[-1])
+summary["max_probable_num"] = get_fish_ID(path_list[-1])
 summary["total files"] = len(path_list)
 summary["missing"] = []
 
 
 previous_fish = ""
-for i in range(summary["max_probable_file"]):
+for i in range(summary["max_probable_num"]):
     
     one_base_iter_num = i+1
     
