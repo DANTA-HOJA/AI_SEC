559669076ac0e00ab51eaa21760bc059e50478ef
r10945018 <rime97410000@gmail.com>
2023-03-25 12:29:48 +0800

    [make var private] Timer

--- modules/misc_utils.py
+++ modules/misc_utils.py
@@ -9,17 +9,17 @@ class Timer():
         pass
     
     def start(self):     
-        self.start_time = time.time()
+        self.__start_time = time.time()
     
     def stop(self): 
-        self.stop_time = time.time()
+        self.__stop_time = time.time()
         
     def calculate_consume_time(self):
-        self.consume_time = self.stop_time - self.start_time
+        self.__consume_time = self.__start_time - self.__start_time
         
     def save_consume_time(self, dir_path, desc):
-        consume_time_str = f"{{ {desc} }}_{{ {self.consume_time:.4f} sec }}"
+        consume_time_str = f"{{ {desc} }}_{{ {self.__consume_time:.4f} sec }}"
         write_path = os.path.normpath(f"{dir_path}/{consume_time_str}")
         # write file
         with open(write_path, mode="w") as f_writer: 
-            f_writer.write(f"{self.consume_time}")
+            f_writer.write(f"{self.__consume_time}")
