98cf28b611645ed7cb01d0af406304d1455aff5c
r10945018 <rime97410000@gmail.com>
2023-04-03 01:56:15 +0800

    [add key 'detection_mode'] collect_BF_results.py

--- data_operate/BrightField/collect_BF_results.py
+++ data_operate/BrightField/collect_BF_results.py
@@ -13,23 +13,29 @@ from norm_name import get_fish_ID_pos
 
 
 # input
-ap_data_root = r"C:\Users\confocal_microscope\Desktop\{PyIJ_OutTest}_BF_Analysis" # r"C:\Users\confocal_microscope\Desktop\WorkingDir\(D2)_Image_AP\{Data}_Data\{20221209_UPDATE_82}_Academia_Sinica_i324"
+ap_data_root = r"C:\Users\confocal_microscope\Desktop\{Temp}_Data\{20230305_NEW_STRUCT}_Academia_Sinica_i409" # r"C:\Users\confocal_microscope\Desktop\WorkingDir\(D2)_Image_AP\{Data}_Data\{20221209_UPDATE_82}_Academia_Sinica_i324"
 analysis_method_desc = "KY_with_NameChecker"
 analysis_root = os.path.join(ap_data_root, f"{{{analysis_method_desc}}}_BF_Analysis")
+detection_mode = "missing" # missing / finding
+
+if not ((detection_mode == "missing") or (detection_mode == "finding")): 
+    raise KeyError("Key: 'detection_mode' can only be 'missing' or 'finding' ")
 
 
 # result
 result_map = {
-	"original_16bit":      "MetaImage/01_original_16bit.tif",
-	"cropped_BF" :         "02_cropped_BF.tif", # CHECK_PT 
-	"AutoThreshold" :      "MetaImage/03_AutoThreshold_*.tif",
-	"measured_mask" :      "MetaImage/04_measured_mask.tif",
-	"cropped_BF--MIX" :     "05_cropped_BF--MIX.tif", # CHECK_PT 
-    "RoiSet" :             "MetaImage/RoiSet.zip",
-	"AutoAnalysis" :       "AutoAnalysis.csv",
-    "ManualAnalysis" :     "ManualAnalysis.csv",
+	"original_16bit" :          "MetaImage/01_original_16bit.tif",
+	"cropped_BF" :              "02_cropped_BF.tif", # CHECK_PT 
+	"AutoThreshold" :           "MetaImage/03_AutoThreshold_*.tif",
+	"measured_mask" :           "MetaImage/04_measured_mask.tif",
+	"cropped_BF--MIX" :         "05_cropped_BF--MIX.tif", # CHECK_PT 
+    "RoiSet" :                  "MetaImage/RoiSet.zip",
+	"AutoAnalysis" :            "AutoAnalysis.csv",
+    "ManualAnalysis" :          "ManualAnalysis.csv",
+    "Manual_measured_mask" :    "Manual_measured_mask.tif",
+    "Manual_cropped_BF--MIX" :  "Manual_cropped_BF--MIX.tif",
 }
-result_key = "AutoAnalysis"
+result_key = "ManualAnalysis"
 
 
 # output
@@ -47,8 +53,7 @@ summary["result_key"] = result_key
 summary["actual_name"] = result_map[result_key]
 summary["max_probable_num"] = get_fish_ID_pos(path_list[-1])[0]
 summary["total files"] = len(path_list)
-if result_key == "ManualAnalysis": summary["finding"] = []
-else:  summary["missing"] = []
+summary[detection_mode] = []
 
 
 previous_fish = ""
@@ -71,15 +76,16 @@ for i in range(summary["max_probable_num"]):
             path = path_list.pop(0)
             resave_result(path, output_dir, result_map[result_key])
             previous_fish = current_name
-            if result_key == "ManualAnalysis": summary["finding"].append(f"{expect_name}")
             
-        else: # 缺號
-            if result_key != "ManualAnalysis": summary["missing"].append(f"{expect_name}")
-            # print("missing : {}".format(summary["missing"][-1]))
+            if detection_mode == "finding": summary[detection_mode].append(f"{expect_name}")
+        
+        else: 
+            if detection_mode == "missing": summary[detection_mode].append(f"{expect_name}")
+            
+        # print(f"{detection_mode} : {summary[detection_mode]}")
 
 
-if result_key == "ManualAnalysis": summary["len(finding)"] = len(summary["finding"])
-else: summary["len(missing)"] = len(summary["missing"])
+summary[f"len({detection_mode})"] = len(summary[detection_mode])
 print(json.dumps(summary, indent=4))
 # Create log writer
 time_stamp = datetime.now().strftime('%Y%m%d_%H_%M_%S')
