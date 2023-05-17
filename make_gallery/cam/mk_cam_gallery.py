82d9d59e45736a54aa10020b950b833e501b38c8
r10945018 <rime97410000@gmail.com>
2023-05-17 23:06:23 +0800

    [improve 'output'] mk_cam_gallery.py

--- make_gallery/cam/mk_cam_gallery.py
+++ make_gallery/cam/mk_cam_gallery.py
@@ -1,16 +1,21 @@
 import os
 import sys
-import time
+from colorama import Fore, Style
 from pathlib import Path
 
-from tqdm.auto import tqdm
-
 sys.path.append("./../../modules/") # add path to scan customized module
+from logger import init_logger
 from gallery.cam.CamGalleryCreator import CamGalleryCreator
 
+# -------------------------------------------------------------------------------------
 
+log = init_logger(r"Make Cam Gallery")
+config_path = Path(r"./../../Config/mk_cam_gallery.toml")
 
-config_path = Path(r"/home/rime97410000/ZebraFish_Code/ZebraFish_AP_POS/make_gallery/cam/mk_cam_gallery.toml")
+# -------------------------------------------------------------------------------------
+
+log.info(f"Start {Fore.BLUE}`{os.path.basename(__file__)}`{Style.RESET_ALL}")
+log.info(f"config_path: {Fore.GREEN}'{config_path.resolve()}'{Style.RESET_ALL}\n")
 
 cam_gallery_creator = CamGalleryCreator(config_path=config_path)
 cam_gallery_creator.run()
