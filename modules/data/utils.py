73f91b33b1356ad258ddb06d85263c572409f1f0
r10945018 <rime97410000@gmail.com>
2023-05-20 08:09:58 +0800

    [move, rename module] dataop.py

---
+++ modules/data/utils.py
@@ -0,0 +1,39 @@
+import os
+import re
+from typing import Tuple, Dict
+
+
+
+def get_fish_ID_pos(string_with_fish_Dname:str) -> Tuple[int, str]:
+    """
+    string_with_fish_Dname:
+    
+        - bf:  .../`20220610_CE001_palmskin_8dpf - Series001_fish_1_BF`/[file_with_extension]
+        - rgb: .../`20220610_CE001_palmskin_8dpf - Series001_fish_1_A_RGB`/[file_with_extension]
+    
+        # TODO:  use `isinstance()` to identify input is a `Path` or `str`
+    """
+    
+    if os.sep in string_with_fish_Dname: 
+        if "MetaImage" in string_with_fish_Dname: fish_name = string_with_fish_Dname.split(os.sep)[-3]
+        elif "reCollection" in string_with_fish_Dname: fish_name = string_with_fish_Dname.split(os.sep)[-1]
+        else: fish_name = string_with_fish_Dname.split(os.sep)[-2]
+    else: fish_name = string_with_fish_Dname
+    
+    fish_name_split = re.split(" |_|-", fish_name)
+    if "BF" in fish_name: assert len(fish_name_split) == 10, f"len(fish_name_list) = '{len(fish_name_split)}', expect '10' "
+    if "RGB" in fish_name: assert len(fish_name_split) == 11, f"len(fish_name_list) = '{len(fish_name_split)}', expect '11' "
+    return int(fish_name_split[8]), fish_name_split[9]
+
+
+
+def create_dict_by_fishID(path_list:list) -> Dict[int, str]:
+    return {get_fish_ID_pos(path)[0] : path for path in path_list}
+
+
+
+def merge_BF_analysis(auto_analysis_dict:Dict[int, str], manual_analysis_dict:Dict[int, str]):
+    for key, value in manual_analysis_dict.items():
+        auto_analysis_dict.pop(key, None)
+        auto_analysis_dict[key] = manual_analysis_dict[key]
+    return auto_analysis_dict
