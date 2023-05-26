708bc05386d9165efdebd4da586ae244c2217c10
r10945018 <rime97410000@gmail.com>
2023-05-26 10:20:31 +0800

    [feature] SinglePredictionParser

---
+++ build_db_xlsx/update_db_xlsx.py
@@ -0,0 +1,41 @@
+import os
+import sys
+from pathlib import Path
+import pandas as pd
+
+rel_module_path = "./../modules/"
+sys.path.append( str(Path(rel_module_path).resolve()) ) # add path to scan customized module
+
+from logger import init_logger
+from db.SinglePredictionParser import SinglePredictionParser
+
+# -----------------------------------------------------------------------------------
+log = init_logger("Updata 'Zebrafish_DB.xlsx'")
+predictions_dir_root = Path(r"/home/rime97410000/ZebraFish_DB/{Model}_Prediction")
+db_name = "ZebraFish_DB"
+
+# -----------------------------------------------------------------------------------
+prediction_dir_list = sorted(list(predictions_dir_root.glob("*")), key=lambda x: str(x).split(os.sep)[-1])
+# prediction_dir_list = prediction_dir_list[:2]
+num = 0
+for prediction_dir in prediction_dir_list: # rm "temp"
+    if str(prediction_dir_list[num]).split(os.sep)[-1] == "temp": prediction_dir_list.pop(num)
+    else: num += 1
+
+zebrafish_db = None
+zebrafish_db_path = predictions_dir_root.joinpath(f"{db_name}.xlsx")
+
+# -----------------------------------------------------------------------------------
+if zebrafish_db_path.exists(): zebrafish_db = pd.read_excel(zebrafish_db_path, index_col=0)
+
+
+for prediction_dir in prediction_dir_list:
+    single_pred_parser = SinglePredictionParser(prediction_dir, log=log)
+    parsed_df = single_pred_parser.parse()
+    if zebrafish_db is None:
+        zebrafish_db = parsed_df
+    else:
+        zebrafish_db = pd.concat([zebrafish_db, parsed_df], ignore_index=True)
+
+
+zebrafish_db.to_excel(zebrafish_db_path)
