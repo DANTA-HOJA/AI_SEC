e6ccc62240efb2623eed94bba097f5b3c5b824bc
r10945018 <rime97410000@gmail.com>
2023-05-27 01:53:47 +0800

    [rename var] update_db_xlsx

--- build_db_xlsx/update_db_xlsx.py
+++ build_db_xlsx/update_db_xlsx.py
@@ -10,11 +10,14 @@ from logger import init_logger
 from db.SinglePredictionParser import SinglePredictionParser
 
 # -----------------------------------------------------------------------------------
-log = init_logger("Updata 'Zebrafish_DB.xlsx'")
-predictions_dir_root = Path(r"/home/rime97410000/ZebraFish_DB/{Model}_Prediction")
-db_name = "ZebraFish_DB"
+
+zebrafish_db_root = Path(r"/home/rime97410000/ZebraFish_DB")
+db_name = r"{DB}_Predictions"
 
 # -----------------------------------------------------------------------------------
+log = init_logger(f"SinglePredictionParser")
+
+predictions_dir_root = zebrafish_db_root.joinpath(r"{Model}_Prediction")
 prediction_dir_list = sorted(list(predictions_dir_root.glob("*")), key=lambda x: str(x).split(os.sep)[-1])
 # prediction_dir_list = prediction_dir_list[:2]
 num = 0
@@ -23,7 +26,7 @@ for prediction_dir in prediction_dir_list: # rm "temp"
     else: num += 1
 
 zebrafish_db = None
-zebrafish_db_path = predictions_dir_root.joinpath(f"{db_name}.xlsx")
+zebrafish_db_path = zebrafish_db_root.joinpath(f"{db_name}.xlsx")
 
 # -----------------------------------------------------------------------------------
 if zebrafish_db_path.exists(): zebrafish_db = pd.read_excel(zebrafish_db_path, index_col=0)
