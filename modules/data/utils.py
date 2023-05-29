9b06e9262ddf1bc1e6a274762c221561fafe711a
r10945018 <rime97410000@gmail.com>
2023-05-30 07:42:41 +0800

    [add TypeCheck] get_fish_ID_pos

--- modules/data/utils.py
+++ modules/data/utils.py
@@ -1,24 +1,28 @@
 import os
 import re
-from typing import Tuple, Dict
+from pathlib import Path
+from typing import Tuple, Dict, Union
 
 
 
-def get_fish_ID_pos(string_with_fish_Dname:str) -> Tuple[int, str]:
+def get_fish_ID_pos(string_with_fish_Dname:Union[str, Path]) -> Tuple[int, str]:
     """
     string_with_fish_Dname:
     
         - bf:  .../`20220610_CE001_palmskin_8dpf - Series001_fish_1_BF`/[file_with_extension]
         - rgb: .../`20220610_CE001_palmskin_8dpf - Series001_fish_1_A_RGB`/[file_with_extension]
     
-        # TODO:  use `isinstance()` to identify input is a `Path` or `str`
     """
     
-    if os.sep in string_with_fish_Dname: 
+    if isinstance(string_with_fish_Dname, Path):
+        string_with_fish_Dname = str(string_with_fish_Dname)
         if "MetaImage" in string_with_fish_Dname: fish_name = string_with_fish_Dname.split(os.sep)[-3]
         elif "reCollection" in string_with_fish_Dname: fish_name = string_with_fish_Dname.split(os.sep)[-1]
         else: fish_name = string_with_fish_Dname.split(os.sep)[-2]
-    else: fish_name = string_with_fish_Dname
+    elif isinstance(string_with_fish_Dname, str): 
+        fish_name = string_with_fish_Dname
+    else: 
+        raise TypeError("unrecognized type of `string_with_fish_Dname`. Only `pathlib.Path` or `str` are accepted.")
     
     fish_name_split = re.split(" |_|-", fish_name)
     if "BF" in fish_name: assert len(fish_name_split) == 10, f"len(fish_name_list) = '{len(fish_name_split)}', expect '10' "
