732a2a63bef02e491469ed515afb58d52668a53b
r10945018 <rime97410000@gmail.com>
2023-05-30 10:30:49 +0800

    [add TypeCheck] resave_result()

--- modules/fileop.py
+++ modules/fileop.py
@@ -1,5 +1,7 @@
 import os
+import shutil
 import filecmp
+from pathlib import Path
 from tqdm.auto import tqdm
 
 
@@ -13,10 +15,20 @@ def create_new_dir(path:str, end="\n", display_in_CLI=True, use_tqdm=False):
 
 
 
-def resave_result(original_path:str, output_dir:str, result_path:str):
+def resave_result(original_path:Path, output_dir:Path, result_name:str):
+    
+    if isinstance(original_path, Path): original_path = str(original_path)
+    else: raise TypeError("'original_path' should be a 'Path' object, please using `from pathlib import Path`")
+    
+    if not isinstance(output_dir, Path):
+        raise TypeError("'output_dir' should be a 'Path' object, please using `from pathlib import Path`")
+    
+    if not isinstance(result_name, str): 
+        raise TypeError("'result_name' should be a 'str' object")
+    
     if "MetaImage" in original_path: fish_name = original_path.split(os.sep)[-3]
     else: fish_name = original_path.split(os.sep)[-2]
-    file_ext = result_path.split(".")[-1]
-    out_path = os.path.join(output_dir, f"{fish_name}.{file_ext}")
-    os.system(f"copy \"{original_path}\" \"{out_path}\" ")
+    file_ext = result_name.split(".")[-1]
+    out_path = output_dir.joinpath(f"{fish_name}.{file_ext}")
+    shutil.copy(original_path, out_path)
     filecmp.cmp(original_path, out_path)
