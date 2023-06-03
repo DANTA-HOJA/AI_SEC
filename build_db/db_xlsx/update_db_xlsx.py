c88c3c2b082ba7613e75283a3b28a6a21e1dfe94
r10945018 <rime97410000@gmail.com>
2023-06-03 17:10:52 +0800

    [update with config] update_db_xlsx.py

--- build_db/db_xlsx/update_db_xlsx.py
+++ build_db/db_xlsx/update_db_xlsx.py
@@ -1,6 +1,7 @@
 import os
 import sys
 from pathlib import Path
+import toml
 import pandas as pd
 
 rel_module_path = "./../../modules/"
@@ -9,36 +10,43 @@ sys.path.append( str(Path(rel_module_path).resolve()) ) # add path to scan custo
 from logger import init_logger
 from db.SinglePredictionParser import SinglePredictionParser
 
-# -----------------------------------------------------------------------------------
+config_dir = Path( "./../../Config/" ).resolve()
 
-zebrafish_db_root = Path(r"/home/rime97410000/ZebraFish_DB")
-db_name = r"{DB}_Predictions"
-
-# -----------------------------------------------------------------------------------
 log = init_logger(f"Build DB Xlsx")
 
-predictions_dir_root = zebrafish_db_root.joinpath(r"{Model}_Prediction")
-prediction_dir_list = sorted(list(predictions_dir_root.glob("*")), key=lambda x: str(x).split(os.sep)[-1])
-# prediction_dir_list = prediction_dir_list[:2]
-num = 0
-for prediction_dir in prediction_dir_list: # rm "temp"
-    if str(prediction_dir_list[num]).split(os.sep)[-1] == "temp": prediction_dir_list.pop(num)
-    else: num += 1
+# -----------------------------------------------------------------------------------
 
-zebrafish_db = None
-zebrafish_db_path = zebrafish_db_root.joinpath(f"{db_name}.xlsx")
+with open(config_dir.joinpath("db_path_plan.toml"), mode="r") as f_reader:
+    dbpp_config = toml.load(f_reader)
+db_root = Path(dbpp_config["root"])
 
 # -----------------------------------------------------------------------------------
-if zebrafish_db_path.exists(): zebrafish_db = pd.read_excel(zebrafish_db_path, index_col=0)
 
+predictions_dir_root = db_root.joinpath(dbpp_config["model_prediction"])
+prediction_dir_list = sorted(list(predictions_dir_root.glob("*")), key=lambda x: str(x).split(os.sep)[-1])
+# prediction_dir_list = prediction_dir_list[:2]
 
-for prediction_dir in prediction_dir_list:
+# rm "temp" directory
+prediction_dir_dict = {str(prediction_dir).split(os.sep)[-1]: prediction_dir
+                                        for prediction_dir in prediction_dir_list}
+prediction_dir_dict.pop("temp")
+
+# -----------------------------------------------------------------------------------
+
+db_xlsx_name = r"{DB}_Predictions"
+db_xlsx_path = db_root.joinpath(f"{db_xlsx_name}.xlsx")
+db_xlsx = None
+
+if db_xlsx_path.exists(): db_xlsx = pd.read_excel(db_xlsx_path, index_col=0)
+
+# -----------------------------------------------------------------------------------
+
+for prediction_dir in prediction_dir_dict.values():
     single_pred_parser = SinglePredictionParser(prediction_dir, log=log)
     parsed_df = single_pred_parser.parse()
-    if zebrafish_db is None:
-        zebrafish_db = parsed_df
+    if db_xlsx is None:
+        db_xlsx = parsed_df
     else:
-        zebrafish_db = pd.concat([zebrafish_db, parsed_df], ignore_index=True)
+        db_xlsx = pd.concat([db_xlsx, parsed_df], ignore_index=True)
 
-
-zebrafish_db.to_excel(zebrafish_db_path)
+db_xlsx.to_excel(db_xlsx_path)
