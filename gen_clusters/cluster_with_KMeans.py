8da5f2c5634669b772a4c1058da2da5a8b924712
r10945018 <rime97410000@gmail.com>
2023-06-14 02:07:26 +0800

    [use 'ProcessedDataInstance'] cluster_with_KMeans.py

--- gen_clusters/cluster_with_KMeans.py
+++ gen_clusters/cluster_with_KMeans.py
@@ -1,26 +1,23 @@
+import os
 import sys
 from pathlib import Path
 import toml
 
-rel_module_path = "./../modules/"
-sys.path.append( str(Path(rel_module_path).resolve()) ) # add path to scan customized module
+abs_module_path = Path("./../modules/").resolve()
+if (abs_module_path.exists()) and (str(abs_module_path) not in sys.path):
+    sys.path.append(str(abs_module_path)) # add path to scan customized module
 
+from data.ProcessedDataInstance import ProcessedDataInstance
 from clustering.SurfaceAreaKMeansCluster import SurfaceAreaKMeansCluster
 
-
-# -----------------------------------------------------------------------------------
-# Load `db_path_plan.toml`
-with open("./../Config/db_path_plan.toml", mode="r") as f_reader:
-    dbpp_config = toml.load(f_reader)
-db_root = Path(dbpp_config["root"])
-
+config_dir = Path( "./../Config/" ).resolve()
 
 # -----------------------------------------------------------------------------------
 # Load `(Cluster)_data.toml`
-with open("./../Config/(Cluster)_data.toml", mode="r") as f_reader:
+with open(config_dir.joinpath("(Cluster)_data.toml"), mode="r") as f_reader:
     config = toml.load(f_reader)
 
-preprocessed_desc = config["data_preprocessed"]["desc"]
+processed_inst_desc = config["data_processed"]["desc"]
 
 if not config["old_classdiv_xlsx"]["full_path"]: old_classdiv_xlsx_path=None
 else: old_classdiv_xlsx_path = Path(config["old_classdiv_xlsx"]["full_path"])
@@ -29,22 +26,16 @@ kmeans_rnd = config["param"]["random_seed"]
 n_clusters = config["param"]["n_clusters"]
 label_str  = config["param"]["label_str"]
 
+# -----------------------------------------------------------------------------------
+# Initialize a `ProcessedDataInstance` object
+
+processed_data_instance = ProcessedDataInstance(config_dir, processed_inst_desc)
+if processed_data_instance.data_xlsx_path:
+    xlsx_path = processed_data_instance.data_xlsx_path
 
 # -----------------------------------------------------------------------------------
-# Generate `path_vars`
-
-# Check `{desc}_Academia_Sinica_i[num]`
-data_preprocessed_root = db_root.joinpath(dbpp_config["data_preprocessed"])
-target_dir_list = list(data_preprocessed_root.glob(f"*{preprocessed_desc}*"))
-assert len(target_dir_list) == 1, (f"[data_preprocessed.desc] in `(Cluster)_data.toml` is not unique/exists, "
-                                   f"find {len(target_dir_list)} possible directories, {target_dir_list}")
-data_preprocessed_dir = target_dir_list[0]
-
-# xlsx: .../{Data}_Preprocessed/{desc}_Academia_Sinica_i[num]/data.xlsx
-xlsx_path = data_preprocessed_dir.joinpath("data.xlsx")
-
-
-# -----------------------------------------------------------------------------------
+# Generate clustered xlsx
+ 
 for arg_6 in [False, True]:
     for arg_7 in [False, True]: # arg_7: x_axis_log_scale = True，產生的 image 會有 kde curve（比較好區分 "原始刻度" 和 "LOG" 刻度）
         SAKMeansCluster = SurfaceAreaKMeansCluster(xlsx_path, n_clusters, label_str, kmeans_rnd,
