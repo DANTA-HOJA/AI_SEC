27c4c5bc37464b04f27e070429c8682bee4688a4
r10945018 <rime97410000@gmail.com>
2023-06-13 17:53:47 +0800

    [combine files] (2 related file) -> collect_results.py

---
+++ data_operate/collect_results.py
@@ -0,0 +1,30 @@
+import os
+import sys
+from pathlib import Path
+import toml
+
+abs_module_path = str(Path("./../modules/").resolve())
+if abs_module_path not in sys.path: sys.path.append(abs_module_path) # add path to scan customized module
+
+from data.ProcessedDataInstance import ProcessedDataInstance
+
+config_dir = Path( "./../Config/" ).resolve()
+
+
+# -----------------------------------------------------------------------------------
+# Load `(CollectResult)_palmskin.toml`
+
+with open(config_dir.joinpath("(CollectResults)_data.toml"), mode="r") as f_reader:
+    config = toml.load(f_reader)
+
+processed_inst_desc = config["data_processed"]["desc"]
+
+processed_name = config["collection"]["processed_name"]
+result_alias = config["collection"]["result_alias"]
+log_mode = config["collection"]["log_mode"]
+
+# -----------------------------------------------------------------------------------
+# Create collection
+
+processed_data_instance = ProcessedDataInstance(config_dir, processed_inst_desc)
+processed_data_instance.collect_results(processed_name, result_alias, log_mode)
