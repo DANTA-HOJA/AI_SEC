bfb814c2d43a7e988d2643d2cddf54b10b1dde7a
r10945018 <rime97410000@gmail.com>
2023-06-16 17:40:03 +0800

    [add arg: dyn_train] create_dataset_xlsx.py

--- dataset_generate_v2/create_dataset_xlsx.py
+++ dataset_generate_v2/create_dataset_xlsx.py
@@ -20,6 +20,7 @@ from data.utils import get_fish_id_pos
 from data.ProcessedDataInstance import ProcessedDataInstance
 from dataset.utils import gen_dataset_param_name, sort_fish_dsname, drop_too_dark, \
                           xlsx_file_name_parser
+from misc.utils import get_target_str_idx_in_list
 from misc.CLIDivider import CLIDivider
 cli_divider = CLIDivider()
 cli_divider.process_start(use_tqdm=True)
@@ -41,6 +42,7 @@ shift_region = config["gen_param"]["shift_region"]
 intensity    = config["gen_param"]["intensity"]
 drop_ratio   = config["gen_param"]["drop_ratio"]
 random_seed  = config["gen_param"]["random_seed"]
+dyn_train    = config["gen_param"]["dynamic_training"]
 random_state = np.random.RandomState(seed=random_seed)
 
 # -----------------------------------------------------------------------------------
@@ -66,8 +68,8 @@ save_dir_Mix_AP = dataset_root.joinpath(instance_name, palmskin_result_alias, "f
 assert save_dir_Mix_AP.exists(), f"{Fore.RED}{Back.BLACK} Cant' find diretory: '{save_dir_Mix_AP}' {Style.RESET_ALL}\n"
 
 # crop_dir_name
-param_name_dict = gen_dataset_param_name(clustered_xlsx_file, crop_size, shift_region, intensity, drop_ratio, 
-                                         random_seed, dict_format=True)
+param_name_dict:dict = gen_dataset_param_name(clustered_xlsx_file, crop_size, shift_region, intensity, drop_ratio, 
+                                              random_seed, dict_format=True)
 crop_dir_name =  f"{param_name_dict['crop_size']}_{param_name_dict['shift_region']}"
 
 # dataset_xlsx_dir
@@ -76,8 +78,9 @@ dataset_xlsx_dir = save_dir_Mix_AP.joinpath(classif_strategy)
 create_new_dir(dataset_xlsx_dir)
 
 # dataset_xlsx_path
-param_name_str = gen_dataset_param_name(clustered_xlsx_file, crop_size, shift_region, intensity, drop_ratio, 
-                                        random_seed, dict_format=False)
+param_name_str:str = gen_dataset_param_name(clustered_xlsx_file, crop_size, shift_region, intensity, drop_ratio, 
+                                            random_seed, dict_format=False)
+if dyn_train: param_name_str = param_name_str.replace(f"{param_name_dict['intensity']}_{param_name_dict['drop_ratio']}", "DYNTRAIN")
 dataset_xlsx_path = dataset_xlsx_dir.joinpath(f"{param_name_str}.xlsx")
 assert not dataset_xlsx_path.exists(), f"{Fore.RED}{Back.BLACK} `dataset_xlsx` already exists: '{dataset_xlsx_path}' {Style.RESET_ALL}\n"
 print(f"dataset_xlsx ( plan to save @ ) : '{dataset_xlsx_path}'\n")
@@ -93,7 +96,11 @@ df_clustered_xlsx: pd.DataFrame = pd.read_excel(clustered_xlsx_path, engine = 'o
 # add column: `fish_id`
 df_clustered_xlsx['fish_id'] = df_clustered_xlsx['Anterior (SP8, .tif)'].apply(lambda x: get_fish_id_pos(x)[0])
 
-img_paths = sorted(save_dir_Mix_AP.glob(f"**/{crop_dir_name}/*.tiff"), key=sort_fish_dsname)
+if dyn_train: 
+    img_paths = []
+    img_paths.extend(sorted(save_dir_Mix_AP.glob(f"train/*/*.tiff"), key=sort_fish_dsname))
+    img_paths.extend(sorted(save_dir_Mix_AP.glob(f"test/*/{crop_dir_name}/*.tiff"), key=sort_fish_dsname))
+else: img_paths = sorted(save_dir_Mix_AP.glob(f"**/{crop_dir_name}/*.tiff"), key=sort_fish_dsname)
 df_dataset = None
 
 pbar = tqdm(total=len(img_paths), desc=f"[ Create Dataset XLSX ] : ")
@@ -103,13 +110,14 @@ for img_path in img_paths:
     img_path_str = str(img_path)
     img_path_split = img_path_str.split(os.sep)
     
-    # crop_name ( image_name )
-    crop_name = img_path_split[-1].split(".")[0]
-    pbar.desc = f"[ Create Dataset XLSX ] {crop_name} : "
+    # image_name
+    image_name = img_path_split[-1].split(".")[0]
+    pbar.desc = f"[ Create Dataset XLSX ] {image_name} : "
     pbar.refresh()
     
     # dsname ( without `crop_idx` )
-    parent_dsname = img_path_split[9]
+    target_idx = get_target_str_idx_in_list(img_path_split, "fish_dataset_horiz_cut_1l2_Mix_AP")
+    parent_dsname = img_path_split[target_idx+2]
     parent_dsname_split = parent_dsname.split("_")
     fish_id     = int(parent_dsname_split[1])
     fish_pos    = parent_dsname_split[2]
@@ -120,25 +128,29 @@ for img_path in img_paths:
     df_filtered_rows = df_filtered_rows.reset_index(drop=True)
     fish_class = df_filtered_rows.loc[0, "class"]
     
-    # preserve / discard
-    img = cv2.imread(img_path_str)
-    select, drop = drop_too_dark([img], intensity, drop_ratio)
-    if select is None: assert drop, "Either `select` or `drop` needs to be empty"
-    if drop is None: assert select, "Either `select` or `drop` needs to be empty"
-    
-    if select:
-        state = "preserve"
-        darkratio = select[0][2]
+    # preserve / discard ( if dyn_train == False )
+    if dyn_train:
+        state = "---"
+        darkratio = "---"
+    else:
+        img = cv2.imread(img_path_str)
+        select, drop = drop_too_dark([img], intensity, drop_ratio)
+        if select is None: assert drop, "Either `select` or `drop` needs to be empty"
+        if drop is None: assert select, "Either `select` or `drop` needs to be empty"
         
-    if drop:
-        state = "discard"
-        darkratio = drop[0][2]
+        if select:
+            state = "preserve"
+            darkratio = select[0][2]
+            
+        if drop:
+            state = "discard"
+            darkratio = drop[0][2]
     
     
     # create `temp_dict`
     temp_dict = {}
     # -------------------------------------------------------
-    temp_dict["crop_name"] = crop_name
+    temp_dict["image_name"] = image_name
     temp_dict["class"] = fish_class
     # -------------------------------------------------------
     temp_dict["parent (dsname)"] = parent_dsname
@@ -146,7 +158,7 @@ for img_path in img_paths:
     temp_dict["fish_pos"]        = fish_pos
     temp_dict["cut_section"]     = cut_section
     # -------------------------------------------------------
-    temp_dict["dataset"]   = img_path_split[8]
+    temp_dict["dataset"]   = img_path_split[target_idx+1]
     temp_dict["darkratio"] = darkratio
     temp_dict["state"]     = state
     # -------------------------------------------------------
