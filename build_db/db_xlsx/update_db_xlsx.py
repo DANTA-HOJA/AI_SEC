ebcef37264bd42668e1caec9aa1e15d8eb0293b0
r10945018 <rime97410000@gmail.com>
2023-06-26 16:06:21 +0800

    [add delete non-existing history] update_db_xlsx.py

--- build_db/db_xlsx/update_db_xlsx.py
+++ build_db/db_xlsx/update_db_xlsx.py
@@ -6,11 +6,15 @@ from pathlib import Path
 import toml
 import pandas as pd
 
-rel_module_path = "./../../modules/"
-sys.path.append( str(Path(rel_module_path).resolve()) ) # add path to scan customized module
+abs_module_path = Path("./../../modules/").resolve()
+if (abs_module_path.exists()) and (str(abs_module_path) not in sys.path):
+    sys.path.append(str(abs_module_path)) # add path to scan customized module
 
 from logger import init_logger
 from db.SinglePredictionParser import SinglePredictionParser
+from misc.CLIDivider import CLIDivider
+cli_divider = CLIDivider()
+cli_divider.process_start(use_tqdm=True)
 
 config_dir = Path( "./../../Config/" ).resolve()
 
@@ -28,13 +32,17 @@ model_prediction_root = db_root.joinpath(dbpp_config["model_prediction"])
 prediction_dir_list = sorted(list(model_prediction_root.glob("*")), key=lambda x: str(x).split(os.sep)[-1])
 # prediction_dir_list = prediction_dir_list[:2]
 
-prediction_dir_dict = {str(prediction_dir).split(os.sep)[-1]: prediction_dir
-                                        for prediction_dir in prediction_dir_list}
-
-# rm "temp" and "un-tested" directory
-for name in list(prediction_dir_dict.keys()):
+prediction_dir_dict = {}
+for prediction_dir in prediction_dir_list:
+    name = str(prediction_dir).split(os.sep)[-1]
     name_split = re.split("{|}", name)
-    if len(name_split) <= 5: prediction_dir_dict.pop(name)
+    if len(name_split) == 9:
+        time_stamp = name_split[0]
+        model_desc = name_split[5]
+        key = f"{time_stamp}_{model_desc}"
+        assert key not in prediction_dir_dict, (f"{Fore.RED}{Back.BLACK} Find multiple histories: time_stamp: '{time_stamp}', model_desc: '{model_desc}'. "
+                                                f"Can not accept duplicate test conditions. {Style.RESET_ALL}\n")
+        prediction_dir_dict.update({key: { "name": name, "path": prediction_dir}})
 
 # -----------------------------------------------------------------------------------
 
@@ -57,12 +65,10 @@ if db_xlsx_path.exists():
 
 # -----------------------------------------------------------------------------------
 
-for name, path in prediction_dir_dict.items():
+for key, value in prediction_dir_dict.items():
     
-    name_split = re.split("{|}", name)
-    time_stamp = name_split[0]
-    model_desc = name_split[5]
-    key = f"{time_stamp}_{model_desc}"
+    name = value["name"]
+    path = value["path"]
     
     single_pred_parser = SinglePredictionParser(path, log=log)
     
@@ -76,6 +82,8 @@ for name, path in prediction_dir_dict.items():
             parsed_df = single_pred_parser.parse()
             parsed_df.index = db_xlsx[(db_xlsx["History Name"] == existing_name)].index
             db_xlsx.loc[parsed_df.index] = parsed_df
+            
+        existing_history_dict.pop(key)
     
     else:
         log.info(f"{Fore.YELLOW} --- New History --- {Style.RESET_ALL}")
@@ -86,4 +94,26 @@ for name, path in prediction_dir_dict.items():
         else:
             db_xlsx = pd.concat([db_xlsx, parsed_df], ignore_index=True)
 
-db_xlsx.to_excel(db_xlsx_path)
+# -----------------------------------------------------------------------------------
+# Delete 'non-existing' history ( directory removed by the user )
+
+for key in existing_history_dict.keys():
+    
+    existing_name = existing_history_dict[key]
+    idx = db_xlsx[(db_xlsx["History Name"] == existing_name)].index
+    db_xlsx = db_xlsx.drop(index=idx)
+    
+    log.info(f"{Fore.YELLOW}Delete : {Fore.MAGENTA} {existing_name} {Style.RESET_ALL}\n")
+
+# -----------------------------------------------------------------------------------
+# Save `db_xlsx`
+
+if db_xlsx.equals(pd.read_excel(db_xlsx_path, engine="openpyxl", index_col=0)):
+    log.info(f"{Fore.YELLOW} --- No Changed --- {Style.RESET_ALL}")
+else:
+    print("Saving `db_xlsx`... ")
+    db_xlsx.to_excel(db_xlsx_path)
+    print(f"{Fore.GREEN}{Back.BLACK} Done! {Style.RESET_ALL}")
+
+# -----------------------------------------------------------------------------------
+cli_divider.process_completed()
