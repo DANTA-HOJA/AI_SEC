a78230bd88966871a93875e687e605deb6198c7e
r10945018 <rime97410000@gmail.com>
2023-08-04 05:51:43 +0800

    [mv, del_fn] fileop.py -> collect.py

--- modules/data/collect.py
+++ modules/data/collect.py
@@ -2,16 +2,6 @@ import os
 import shutil
 import filecmp
 from pathlib import Path
-from tqdm.auto import tqdm
-
-
-
-def create_new_dir(path:str, end="\n", display_in_CLI=True, use_tqdm=False):
-    if not os.path.exists(path):
-        # if the demo_folder directory is not exist then create it.
-        os.makedirs(path)
-        if use_tqdm: tqdm.write(f"path: '{path}' is created!{end}")
-        elif display_in_CLI: print(f"path: '{path}' is created!{end}")
 
 
 
@@ -23,7 +13,7 @@ def resave_result(original_path:Path, output_dir:Path, result_name:str):
     if not isinstance(output_dir, Path):
         raise TypeError("'output_dir' should be a 'Path' object, please using `from pathlib import Path`")
     
-    if not isinstance(result_name, str): 
+    if not isinstance(result_name, str):
         raise TypeError("'result_name' should be a 'str' object")
     
     if "MetaImage" in original_path: fish_name = original_path.split(os.sep)[-3]
