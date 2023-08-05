ff5266303dd2e0d6570bf4be748471b74452ddc4
r10945018 <rime97410000@gmail.com>
2023-08-05 19:19:25 +0800

    [add cli_out] get_repo_root()

--- modules/misc/utils.py
+++ modules/misc/utils.py
@@ -60,15 +60,20 @@ def get_target_str_idx_in_list(source_list:List[str], target_str:str) -> Union[i
 
 
 
-def get_repo_root() -> Path:
+def get_repo_root(logger:Logger=None) -> Path:
     """ TODO
     """
+    cli_out = decide_cli_output(logger)
+    
     path_split = os.path.abspath(".").split(os.sep)
     target_idx = get_target_str_idx_in_list(path_split, "ZebraFish_AP_POS")
     assert_run_under_repo_root(target_idx)
-    repo_root = os.sep.join(path_split[:target_idx+1])
     
-    return Path(repo_root)
+    """ generate path """
+    repo_root = Path(os.sep.join(path_split[:target_idx+1]))
+    cli_out(f"Repository: '{repo_root}'")
+    
+    return repo_root
 
 
 
