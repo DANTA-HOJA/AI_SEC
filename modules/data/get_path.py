0a9324ef11f8a2dbb9d775106021b5996177e3bb
r10945018 <rime97410000@gmail.com>
2023-08-06 04:35:11 +0800

    [new_fn] 2 new functions

---
+++ modules/data/get_path.py
@@ -0,0 +1,47 @@
+import os
+import sys
+import re
+from pathlib import Path
+from typing import List, Dict, Tuple, Union
+from logging import Logger
+from tomlkit.toml_document import TOMLDocument
+
+from ..assert_fn import *
+from ..misc.utils import decide_cli_output
+
+
+def get_fiji_local_path(dbpp_config:Union[dict, TOMLDocument], logger:Logger=None) -> Path:
+    """
+    """
+    cli_out = decide_cli_output(logger)
+    
+    fiji_local = Path(dbpp_config["fiji_local"])
+    assert_dir_exists(fiji_local)
+    cli_out(f"Fiji Local: '{fiji_local}'")
+    
+    return str(fiji_local)
+
+
+
+def get_lif_scan_root(dbpp_config:Union[dict, TOMLDocument], 
+                      config:Union[dict, TOMLDocument], logger:Logger=None):
+    """
+    """
+    cli_out = decide_cli_output(logger)
+    
+    """ dbpp_config keywords """
+    db_root = Path(dbpp_config["root"])
+    assert_dir_exists(db_root)
+    data_nasdl = dbpp_config["data_nasdl"]
+    
+    """ config keywords """
+    nasdl_dir = config["data_nasdl"]["dir"]
+    nasdl_type    = config["data_nasdl"]["type"]
+    
+    """ Generate path """
+    lif_scan_root = db_root.joinpath(data_nasdl, nasdl_dir, nasdl_type)
+    assert_dir_exists(db_root)
+    cli_out(f"LIF Scan Root: '{lif_scan_root}'")
+    
+    return lif_scan_root
+    
