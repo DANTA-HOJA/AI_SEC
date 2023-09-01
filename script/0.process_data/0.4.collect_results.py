51ce2a980953f6ac6778a580b15ba663362624d2
r10945018 <rime97410000@gmail.com>
2023-09-01 22:09:31 +0800

    [file: update] 0.4.collect_results.py

--- script/0.process_data/0.4.collect_results.py
+++ script/0.process_data/0.4.collect_results.py
@@ -1,30 +1,23 @@
-import os
 import sys
 from pathlib import Path
-import toml
 
-abs_module_path = Path("./../modules/").resolve()
+abs_module_path = Path("./../../").resolve()
 if (abs_module_path.exists()) and (str(abs_module_path) not in sys.path):
     sys.path.append(str(abs_module_path)) # add path to scan customized module
 
-from data.ProcessedDataInstance import ProcessedDataInstance
+from modules.shared.utils import get_repo_root, load_config
+from modules.data.processeddatainstance import ProcessedDataInstance
 
-config_dir = Path( "./../Config/" ).resolve()
+""" Detect Repository """
+repo_root = get_repo_root(display_on_CLI=True)
 
-# -----------------------------------------------------------------------------------
-config_name = "(CollectResults)_data.toml"
-
-with open(config_dir.joinpath(config_name), mode="r") as f_reader:
-    config = toml.load(f_reader)
-
-processed_inst_desc = config["data_processed"]["desc"]
-
-processed_name = config["collection"]["processed_name"]
+""" Get variable """
+config = load_config("0.4.collect_results.toml")
+image_type = config["collection"]["image_type"]
 result_alias = config["collection"]["result_alias"]
 log_mode = config["collection"]["log_mode"]
 
-# -----------------------------------------------------------------------------------
-# Create collection
 
-processed_data_instance = ProcessedDataInstance(config_dir, processed_inst_desc)
-processed_data_instance.collect_results(processed_name, result_alias, log_mode)
+processed_data_instance = ProcessedDataInstance()
+processed_data_instance.load_config("0.4.collect_results.toml")
+processed_data_instance.collect_results(image_type, result_alias, log_mode)
