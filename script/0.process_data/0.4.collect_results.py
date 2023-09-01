7876bde213e7921c6073ad3514b28af44f491e10
r10945018 <rime97410000@gmail.com>
2023-09-02 04:51:23 +0800

    [file: rm_import] 0.4.collect_results.py

--- script/0.process_data/0.4.collect_results.py
+++ script/0.process_data/0.4.collect_results.py
@@ -5,7 +5,7 @@ abs_module_path = Path("./../../").resolve()
 if (abs_module_path.exists()) and (str(abs_module_path) not in sys.path):
     sys.path.append(str(abs_module_path)) # add path to scan customized module
 
-from modules.shared.utils import get_repo_root, load_config
+from modules.shared.utils import get_repo_root
 from modules.data.processeddatainstance import ProcessedDataInstance
 
 """ Detect Repository """
