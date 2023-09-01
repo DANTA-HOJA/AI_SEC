208c64f80dcadb3ff51eea1f8d24b5a993ed274b
r10945018 <rime97410000@gmail.com>
2023-09-02 04:48:27 +0800

    [file: update] 0.4.collect_results.py

--- script/0.process_data/0.4.collect_results.py
+++ script/0.process_data/0.4.collect_results.py
@@ -15,9 +15,8 @@ repo_root = get_repo_root(display_on_CLI=True)
 processed_data_instance = ProcessedDataInstance()
 processed_data_instance.load_config("0.4.collect_results.toml")
 
-""" Get variables """
+""" Main Process """
 image_type   = processed_data_instance.config["collection"]["image_type"]
 result_alias = processed_data_instance.config["collection"]["result_alias"]
 log_mode     = processed_data_instance.config["collection"]["log_mode"]
-
 processed_data_instance.collect_results(image_type, result_alias, log_mode)
