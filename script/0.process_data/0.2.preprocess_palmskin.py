1647941a1c4f7cc21854bf66f7368069e90def49
r10945018 <rime97410000@gmail.com>
2023-09-02 14:59:34 +0800

    [module: mv] PalmskinPreprocesser

--- script/0.process_data/0.2.preprocess_palmskin.py
+++ script/0.process_data/0.2.preprocess_palmskin.py
@@ -5,7 +5,7 @@ abs_module_path = Path("./../../").resolve()
 if (abs_module_path.exists()) and (str(abs_module_path) not in sys.path):
     sys.path.append(str(abs_module_path)) # add path to scan customized module
 
-from modules.data.palmskinpreprocesser import PalmskinPreprocesser
+from modules.data.lif.palmskinpreprocesser import PalmskinPreprocesser
 from modules.shared.utils import get_repo_root
 
 """ Detect Repository """
