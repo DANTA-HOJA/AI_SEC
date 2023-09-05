0fec9b69bbfe3f8e754dfb5905328c39e7ac7a30
r10945018 <rime97410000@gmail.com>
2023-09-06 02:14:13 +0800

    [script: misc_changed] 0.3.analyze_brightfield.py

--- script/0.process_data/0.3.analyze_brightfield.py
+++ script/0.process_data/0.3.analyze_brightfield.py
@@ -9,7 +9,7 @@ from modules.data.lif.brightfieldanalyzer import BrightfieldAnalyzer
 from modules.shared.utils import get_repo_root
 
 """ Detect Repository """
-repo_root = get_repo_root(display_on_CLI=True)
+print(f"Repository: '{get_repo_root()}'")
 
 
 brightfield_analyzer = BrightfieldAnalyzer()
