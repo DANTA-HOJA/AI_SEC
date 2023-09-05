e6e2789015933b55b0ee682c473ef32fe27e37a5
r10945018 <rime97410000@gmail.com>
2023-09-05 23:37:45 +0800

    [script: misc_changed] 0.1.check_lif_name.py

--- script/0.process_data/0.1.check_lif_name.py
+++ script/0.process_data/0.1.check_lif_name.py
@@ -9,7 +9,7 @@ from modules.data.lif.batchlifnamechecker import BatchLIFNameChecker
 from modules.shared.utils import get_repo_root
 
 """ Detect Repository """
-repo_root = get_repo_root(display_on_CLI=True)
+print(f"Repository: '{get_repo_root()}'") 
 
 
 batch_lif_name_checker = BatchLIFNameChecker()
