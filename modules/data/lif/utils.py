a48a713e26923aac3b68c56627f5fddb9fd92efc
r10945018 <rime97410000@gmail.com>
2023-09-05 19:43:50 +0800

    [module: adj cli_out] lif.utils

--- modules/data/lif/utils.py
+++ modules/data/lif/utils.py
@@ -6,15 +6,15 @@ from typing import List, Dict, Tuple, Union
 from logging import Logger
 import json
 
-from ...shared.utils import decide_cli_output
+from ...shared.clioutput import CLIOutput
+
 from ...assert_fn import *
 
 
-def scan_lifs_under_dir(dir:Path, batches:list, logger:Logger=None) -> List[str]:
+
+def scan_lifs_under_dir(dir:Path, batches:list, cli_out:CLIOutput=None) -> List[str]:
     """ Scan Leica LIF file
     """
-    cli_out = decide_cli_output(logger)
-    
     assert_dir_exists(dir)
     lif_path_list = []
     
@@ -29,8 +29,9 @@ def scan_lifs_under_dir(dir:Path, batches:list, logger:Logger=None) -> List[str]
     lif_path_list.sort(key=lambda x: x.split(os.sep)[-1])
     
     """ CLI output """
-    formatted = json.dumps(lif_path_list, indent=4)
-    cli_out(f'lif_path_list {type(lif_path_list)}: {formatted}')
-    cli_out(f"[ found {len(lif_path_list)} lif files ]")
+    if cli_out:
+        formatted = json.dumps(lif_path_list, indent=4)
+        cli_out.write(f'lif_path_list {type(lif_path_list)}: {formatted}')
+        cli_out.write(f"[ found {len(lif_path_list)} lif files ]")
     
     return lif_path_list
