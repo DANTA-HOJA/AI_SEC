7833c097058df6fdd4577fa50342ebccc9eb5658
r10945018 <rime97410000@gmail.com>
2023-09-06 14:34:27 +0800

    [script: replace] 0.3.cp_manual.ipynb -> 0.3.cp_manual_analysis.py

---
+++ script/0.process_data/0.3.cp_manual_analysis/0.3.cp_manual_analysis.py
@@ -0,0 +1,45 @@
+import sys
+from pathlib import Path
+import shutil
+
+abs_module_path = Path("./../../../").resolve()
+if (abs_module_path.exists()) and (str(abs_module_path) not in sys.path):
+    sys.path.append(str(abs_module_path)) # add path to scan customized module
+
+from modules.data.processeddatainstance import ProcessedDataInstance
+from modules.shared.clioutput import CLIOutput
+from modules.shared.utils import get_repo_root
+
+""" Detect Repository """
+print(f"Repository: '{get_repo_root()}'")
+
+""" CLI output """
+cli_out = CLIOutput()
+
+""" Old Instance ( with manual analysis results ) """
+cli_out.divide()
+old_instance = ProcessedDataInstance()
+old_instance.load_config(Path("old_instance.toml"))
+old_dict = old_instance.brightfield_processed_dname_dirs_dict
+
+""" New Instance """
+cli_out.divide()
+new_instance = ProcessedDataInstance()
+new_instance.load_config(Path("new_instance.toml"))
+new_dict = new_instance.brightfield_processed_dname_dirs_dict
+
+
+""" Main Process """
+cli_out.divide()
+filenames = ["Manual_cropped_BF--MIX.tif", "Manual_measured_mask.tif", "ManualAnalysis.csv"]
+
+for key, value in new_dict.items():
+    
+    for filename in filenames:
+        
+        new_path = value.joinpath(filename)
+        old_path = old_dict[key].joinpath(filename)
+        
+        if not new_path.exists():
+            shutil.copy(old_path, new_path)
+            print(f"'{old_path}' -> '{new_path}'")
