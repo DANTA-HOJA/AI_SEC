b934e9e0079f5d10954113afb478f587b8e352f3
r10945018 <rime97410000@gmail.com>
2023-09-06 10:52:17 +0800

    [class: adj cli_out] BrightfieldAnalyzer

--- modules/data/lif/brightfieldanalyzer.py
+++ modules/data/lif/brightfieldanalyzer.py
@@ -9,11 +9,11 @@ import toml
 import tomlkit
 from tomlkit.toml_document import TOMLDocument
 
-from ..ij.zfij import ZFIJ
 from .utils import scan_lifs_under_dir
-from ...shared.logger import init_logger
-from ...shared.utils import load_config, create_new_dir
+from ..ij.zfij import ZFIJ
+from ...shared.clioutput import CLIOutput
 from ...shared.pathnavigator import PathNavigator
+from ...shared.utils import load_config, create_new_dir
 
 from ...assert_fn import *
 
@@ -21,25 +21,20 @@ from ...assert_fn import *
 
 class BrightfieldAnalyzer():
     
-    def __init__(self, zfij_instance:ZFIJ=None) -> None:
+    def __init__(self, zfij_instance:ZFIJ=None, display_on_CLI=True) -> None:
         """ Brightfield image analyzing
         """
-        
         # Initialize `Fiji` ( This will change the working directory to where the `JVM` exists. )
         if zfij_instance:
             self._zfij = zfij_instance
         else:
-            self._zfij = ZFIJ()
-        
-        """ Logger """
-        self._logger = init_logger(r"Analyze Brightfield")
-        self._display_kwargs = {
-            "display_on_CLI": True,
-            "logger": self._logger
-        }
+            self._zfij = ZFIJ(display_on_CLI)
         
         self._path_navigator = PathNavigator()
         self._sn_digits = "02"
+        
+        """ CLI output """
+        self._cli_out = CLIOutput(display_on_CLI, logger_name="Analyze Brightfield")
         
         self._reset_attrs()
         self._reset_single_img_attrs()
@@ -59,9 +54,9 @@ class BrightfieldAnalyzer():
     
     
     
-    def run(self):
+    def run(self, config_file:Union[str, Path]="0.3.analyze_brightfield.toml"):
         """ Actions
-        1. Load config: `0.3.analyze_brightfield.toml`
+        1. Load config
         2. Source: 
             1. Get `lif_scan_root`
             2. Scan `LIF` files
@@ -74,29 +69,33 @@ class BrightfieldAnalyzer():
         4. Analyze brightfield images
         5. Close `LOG_FILE` (3.2)
         6. Save `alias_map` as toml file under `brightfield_processed_dir`
+        
+        Args:
+            config_file (Union[str, Path], optional): Defaults to `0.3.analyze_brightfield.toml`.
         """
         self._reset_attrs()
+        self._cli_out.divide()
         
         """ STEP 1. Load config """
-        config = load_config("0.3.analyze_brightfield.toml", **self._display_kwargs)
+        config = load_config(config_file, cli_out=self._cli_out)
         nasdl_batches = config["data_nasdl"]["batches"]
         
         """ STEP 2. Source """
-        lif_scan_root = self._path_navigator.raw_data.get_lif_scan_root(config, **self._display_kwargs)
-        lif_path_list = scan_lifs_under_dir(lif_scan_root, nasdl_batches, logger=self._logger)
+        lif_scan_root = self._path_navigator.raw_data.get_lif_scan_root(config, self._cli_out)
+        lif_path_list = scan_lifs_under_dir(lif_scan_root, nasdl_batches, self._cli_out)
         self.total_lif_file = len(lif_path_list)
         
         """ STEP 3. Destination """
         """ 3.1. """
-        self.brightfield_processed_dir = self._path_navigator.processed_data.get_processed_dir("brightfield", config, **self._display_kwargs)
+        self.brightfield_processed_dir = self._path_navigator.processed_data.get_processed_dir("brightfield", config, self._cli_out)
         if self.brightfield_processed_dir:
             self.mode = "UPDATE"
         else:
-            instance_root = self._path_navigator.processed_data.get_instance_root(config, **self._display_kwargs)
+            instance_root = self._path_navigator.processed_data.get_instance_root(config, self._cli_out)
             reminder = config["data_processed"]["brightfield_reminder"]
             self.brightfield_processed_dir = instance_root.joinpath(f"{{{reminder}}}_BrightField_analyze")
-            create_new_dir(self.brightfield_processed_dir, display_on_CLI=False)
-        self._logger.info(f"Process Mode : {self.mode}")
+            create_new_dir(self.brightfield_processed_dir)
+        self._cli_out.write(f"Process Mode : {self.mode}")
         
         """ 3.2. """
         cp_config_path = self.brightfield_processed_dir.joinpath("brightfield_analyze_config.toml")
@@ -121,7 +120,7 @@ class BrightfieldAnalyzer():
             self._single_lif_preprocess(lif_path)
         
         self.log_writer.write(f"{'-'*40}  finished  {'-'*40} \n")
-        self._logger.info(" -- finished -- ")
+        self._cli_out.write(" -- finished -- ")
         
         """ STEP 5. Close `LOG_FILE` """
         self.log_writer.close()
@@ -146,8 +145,8 @@ class BrightfieldAnalyzer():
         """
         """
         """ Display info """
-        self._logger.info(f"Processing ... {self.lif_enum}/{self.total_lif_file}")
-        self._logger.info(f"LIF_FILE : {lif_path}")
+        self._cli_out.write(f"Processing ... {self.lif_enum}/{self.total_lif_file}")
+        self._cli_out.write(f"LIF_FILE : '{lif_path}'")
         
         """ Write `LOG_FILE` """
         self.log_writer.write(f"|{'-'*40}  Processing ... {self.lif_enum}/{self.total_lif_file}  {'-'*40} \n")
@@ -200,9 +199,9 @@ class BrightfieldAnalyzer():
             dim2_elem = img.getNumericProperty("Image #0|DimensionDescription #2|NumberOfElements")
             dim2_unit = dim2_length/(dim2_elem-1)*(10**6)
             assert dim1_unit == dim2_unit, f"Voxel_X != Voxel_Y, Voxel_X, Voxel_Y = ({dim1_unit}, {dim2_unit}) micron/pixel"
-            self._logger.info(f"series {series_num:{len(str(series_cnt))}}/{series_cnt} : '{comb_name}' , "
-                              f"Dimensions : {img_dimensions} ( width, height, channels, slices, frames ), "
-                              f"Voxel_X_Y : {dim1_unit:.4f} micron")
+            self._cli_out.write(f"series {series_num:{len(str(series_cnt))}}/{series_cnt} : '{comb_name}' , "
+                                f"Dimensions : {img_dimensions} ( width, height, channels, slices, frames ), "
+                                f"Voxel_X_Y : {dim1_unit:.4f} micron")
             
             """ Write `LOG_FILE` """
             self.log_writer.write(f"|-- processing ...  series {series_num:{len(str(series_cnt))}}/{series_cnt} in {self.lif_enum}/{self.total_lif_file} \n")
@@ -215,9 +214,9 @@ class BrightfieldAnalyzer():
             else:
                 self.save_root = self.brightfield_processed_dir.joinpath(comb_name)
             assert_dir_not_exists(self.save_root)
-            create_new_dir(self.save_root, display_on_CLI=False)
+            create_new_dir(self.save_root)
             self.metaimg_dir = self.save_root.joinpath("MetaImage")
-            create_new_dir(self.metaimg_dir, display_on_CLI=False)
+            create_new_dir(self.metaimg_dir)
             
             
             """ Do preprocess """
@@ -243,8 +242,8 @@ class BrightfieldAnalyzer():
                     self.save_measured_result()
                 else:
                     """ logger and log_file """
-                    self._logger.info(f"      ROI in RoiManager: {roi_cnt}")
-                    self._logger.info(f"      #### ERROR : Number of ROI not = 1")
+                    self._cli_out.write(f"      ROI in RoiManager: {roi_cnt}")
+                    self._cli_out.write(f"      #### ERROR : Number of ROI not = 1")
                     # Write Log
                     self.log_writer.write(f"|         number of ROI = {roi_cnt} \n")
                     self.log_writer.write("| #### ERROR : Number of ROI not = 1 \n")
@@ -253,7 +252,7 @@ class BrightfieldAnalyzer():
             self._zfij.reset_all_window()
             self.log_writer.write(f"| \n") # make Log file looks better.
         
-        self._logger.info("\n") # make CLI output prettier
+        self._cli_out.write("\n") # make CLI output prettier
         self.log_writer.write("\n\n\n")
     
     
@@ -280,7 +279,7 @@ class BrightfieldAnalyzer():
         slices = img_dimensions[3]
         
         if slices > 1:
-            self._logger.info("      #### WARNING : Number of Slices > 1, run ' Find focused slices ' ") # WARNING:
+            self._cli_out.write("      #### WARNING : Number of Slices > 1, run ' Find focused slices ' ") # WARNING:
             self.log_writer.write("| #### WARNING : Number of Slices > 1, run ' Find focused slices ' \n") # WARNING:
             self._zfij.run(img, "Find focused slices", "select=100 variance=0.000 select_only")
             img = self._zfij.ij.WindowManager.getCurrentImage()
