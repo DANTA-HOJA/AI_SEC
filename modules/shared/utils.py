0e7df837237bbe9b8e3dd34adfb6ffd2ab1bf164
r10945018 <rime97410000@gmail.com>
2023-09-06 13:40:23 +0800

    [fn: adj docstring] load_config()

--- modules/shared/utils.py
+++ modules/shared/utils.py
@@ -81,7 +81,7 @@ def load_config(config_file:Union[str, Path], reserve_comment:bool=False,
         cli_out (CLIOutput, optional): a `CLIOutput` object. Defaults to None.
 
     Raises:
-        NotImplementedError: `config_file` accept `str` or `Path` only.
+        NotImplementedError: argument `config_file` should be `str` or `Path` object.
 
     Returns:
         Union[dict, TOMLDocument]: a toml config
