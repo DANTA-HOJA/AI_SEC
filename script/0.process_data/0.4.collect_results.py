ea9e9ea011e3908c80099c1aece30b50019dacfb
r10945018 <rime97410000@gmail.com>
2023-09-06 13:56:23 +0800

    [script: update] 2 related script

--- script/0.process_data/0.4.collect_results.py
+++ script/0.process_data/0.4.collect_results.py
@@ -5,18 +5,12 @@ abs_module_path = Path("./../../").resolve()
 if (abs_module_path.exists()) and (str(abs_module_path) not in sys.path):
     sys.path.append(str(abs_module_path)) # add path to scan customized module
 
-from modules.shared.utils import get_repo_root
 from modules.data.processeddatainstance import ProcessedDataInstance
+from modules.shared.utils import get_repo_root
 
 """ Detect Repository """
-repo_root = get_repo_root(display_on_CLI=True)
+print(f"Repository: '{get_repo_root()}'")
 
 
 processed_data_instance = ProcessedDataInstance()
-processed_data_instance.load_config("0.4.collect_results.toml")
-
-""" Main Process """
-image_type   = processed_data_instance.config["collection"]["image_type"]
-result_alias = processed_data_instance.config["collection"]["result_alias"]
-log_mode     = processed_data_instance.config["collection"]["log_mode"]
-processed_data_instance.collect_results(image_type, result_alias, log_mode)
+processed_data_instance.collect_results()
--- script/0.process_data/0.5.create_data_xlsx.py
+++ script/0.process_data/0.5.create_data_xlsx.py
@@ -5,15 +5,12 @@ abs_module_path = Path("./../../").resolve()
 if (abs_module_path.exists()) and (str(abs_module_path) not in sys.path):
     sys.path.append(str(abs_module_path)) # add path to scan customized module
 
-from modules.shared.utils import get_repo_root
 from modules.data.processeddatainstance import ProcessedDataInstance
+from modules.shared.utils import get_repo_root
 
 """ Detect Repository """
-repo_root = get_repo_root(display_on_CLI=True)
+print(f"Repository: '{get_repo_root()}'")
 
 
 processed_data_instance = ProcessedDataInstance()
-processed_data_instance.load_config("0.5.create_data_xlsx.toml")
-
-""" Main Process """
 processed_data_instance.create_data_xlsx()
