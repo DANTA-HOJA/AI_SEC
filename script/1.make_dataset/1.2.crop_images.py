8c3409b678a87ceb35b188a77f79cf81dd67b4ce
r10945018 <rime97410000@gmail.com>
2023-09-15 09:21:21 +0800

    [script: new_file] 1.2.crop_images.py

---
+++ script/1.make_dataset/1.2.crop_images.py
@@ -0,0 +1,16 @@
+import sys
+from pathlib import Path
+
+abs_module_path = Path("./../../").resolve()
+if (abs_module_path.exists()) and (str(abs_module_path) not in sys.path):
+    sys.path.append(str(abs_module_path)) # add path to scan customized module
+
+from modules.dataset.imagecropper import ImageCropper
+from modules.shared.utils import get_repo_root
+
+""" Detect Repository """
+print(f"Repository: '{get_repo_root()}'")
+
+
+image_cropper = ImageCropper()
+image_cropper.run()
