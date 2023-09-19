71729aec0e1c35cf399629a1e56dcbafc733e8a9
r10945018 <rime97410000@gmail.com>
2023-09-19 22:18:01 +0800

    [module: new_class] ImgDataset

---
+++ modules/dl/dataset/imgdataset.py
@@ -0,0 +1,87 @@
+from typing import List, Dict, Tuple, Union
+
+import cv2
+import numpy as np
+import pandas as pd
+from imgaug import augmenters as iaa
+import torch
+from torch.utils.data import Dataset
+
+from ...shared.clioutput import CLIOutput
+# -----------------------------------------------------------------------------/
+
+
+class ImgDataset(Dataset):
+    """ Use `dataset_xlsx` to get the training properties
+        
+        P.S. Old vesion is using `image_path` directly
+    """
+    def __init__(self, mode:str, name_list:List[str], 
+                 name_dict:dict, class2num_dict:Dict[str, int],
+                 resize:int, use_hsv:bool, transform:iaa.Sequential=None, 
+                 display_on_CLI=True) -> None:
+        """
+        """
+        # ---------------------------------------------------------------------
+        # """ components """
+        
+        self._cli_out = CLIOutput(display_on_CLI, 
+                                  logger_name=f"{mode.capitalize()} Dataset")
+        
+        # ---------------------------------------------------------------------
+        # """ attributes """
+        self.mode = mode
+        self.name_list = name_list
+        self.name_dict = name_dict
+        self.class2num_dict = class2num_dict
+        self.resize = resize # (W, H), square image, W == H
+        self.use_hsv = use_hsv
+        self.transform = transform
+        
+        if self.use_hsv is True: self._cli_out.write("※ : using 'HSV' when getting images from the dataset")
+        if self.transform is not None: self._cli_out.write("※ : applying augmentation on the fly")
+        # ---------------------------------------------------------------------/
+
+
+
+    def __len__(self):
+        """
+        """
+        return len(self.name_list)
+        # ---------------------------------------------------------------------/
+
+
+
+    def __getitem__(self, index):
+        """
+        """
+        """ Get name """
+        name = self.name_list[index]
+        
+        """ Read image """
+        fish_path = self.name_dict[name]["path"]
+        img = cv2.imread(str(fish_path))
+        
+        """ Augmentation on the fly """
+        if self.transform is not None:
+            img = self.transform(image=img)
+        
+        """ Choosing the color space, 'RGB' or 'HSV' """
+        if self.use_hsv is True: img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV_FULL) # BGR -> HSV
+        else: img = img[:,:,::-1] # BGR -> RGB
+        
+        """ Resize and rearrange to the size of model input """
+        img = cv2.resize(img, self.resize, interpolation=cv2.INTER_CUBIC)
+        img = img / 255.0 # normalize to 0~1
+        img = np.moveaxis(img, -1, 0) # img_dims == 3: (H, W, C) -> (C, H, W)
+        
+        """ Read class label """
+        fish_class = self.name_dict[name]["class"]
+        cls_idx = self.class2num_dict[fish_class]
+
+        """ To `Tensor` """
+        img = torch.from_numpy(img).float()  # 32-bit float
+        cls_idx = torch.tensor(cls_idx) # 64-bit int, can be [0] or [1] or [2] only
+        
+        return img, cls_idx, name
+        # ---------------------------------------------------------------------/    
