d13f41f803a50ea5e0a9bed85415b5c379660f7e
r10945018 <rime97410000@gmail.com>
2023-09-20 08:59:35 +0800

    [fn: adj var] set_gpu

--- modules/dl/utils.py
+++ modules/dl/utils.py
@@ -15,11 +15,11 @@ def set_gpu(cuda_idx:int, cli_out:CLIOutput=None):
     if not torch.cuda.is_available():
         raise RuntimeError("Can't find any GPU")
     
-    device = torch.device("cuda")
+    device = torch.device(f"cuda:{cuda_idx}")
     
-    torch.cuda.set_device(cuda_idx)
-    device_num = torch.cuda.current_device()
-    device_name = torch.cuda.get_device_name(device_num)
+    # torch.cuda.set_device(cuda_idx)
+    # device_num = torch.cuda.current_device()
+    device_name = torch.cuda.get_device_name(device)
     torch.cuda.empty_cache()
     
     if cli_out: cli_out.write(f"Using '{device}', device_name = '{device_name}'")
