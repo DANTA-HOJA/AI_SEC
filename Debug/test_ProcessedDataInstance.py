36abd729a73a998a8ade3579baf63ac9123cab72
r10945018 <rime97410000@gmail.com>
2023-10-14 16:33:50 +0800

    [test_file: new_file] test_ProcessedDataInstance.py

---
+++ Debug/test_ProcessedDataInstance.py
@@ -0,0 +1,34 @@
+import os
+import sys
+import re
+from pathlib import Path
+import json
+
+abs_module_path = Path("./../").resolve()
+if (abs_module_path.exists()) and (str(abs_module_path) not in sys.path):
+    sys.path.append(str(abs_module_path)) # add path to scan customized module
+
+from modules.shared.config import load_config
+from modules.data.processeddatainstance import ProcessedDataInstance
+
+processed_data_instance = ProcessedDataInstance()
+processed_data_instance.set_attrs("0.5.create_data_xlsx.toml")
+
+# reminder
+print(f"palmskin_processed_reminder: '{processed_data_instance.palmskin_processed_reminder}'")
+print(f"brightfield_processed_reminder: '{processed_data_instance.brightfield_processed_reminder}'\n")
+
+# clustered_xlsx_files
+temp_dict = {key: str(value) for key, value in processed_data_instance.clustered_xlsx_files_dict.items()}
+print(f">>> Clustered XLSX Files: \n{json.dumps(temp_dict, indent=2)}\n")
+
+# alias_map
+alias_map = processed_data_instance.brightfield_processed_alias_map
+print(f">>> Brightfield Alias Map [ {len(alias_map)} files ]: \n{json.dumps(alias_map, indent=2)}\n")
+alias_map = processed_data_instance.palmskin_processed_alias_map
+print(f">>> Palmskin Alias Map [ {len(alias_map)} files ]: \n{json.dumps(alias_map, indent=2)}\n")
+
+# gtarget results
+rel_path, result_paths = processed_data_instance._get_sorted_results("palmskin", "RGB_fusion")
+result_paths = [str(result_path) for result_path in result_paths]
+print(f">>> Relative Path to `dname_dir`: {rel_path} [ found {len(result_paths)} items ] \n{json.dumps(result_paths, indent=2)}\n")
