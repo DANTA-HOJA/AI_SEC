396d0b8b80aed124d185e9b27466440bcc2ba75c
r10945018 <rime97410000@gmail.com>
2023-10-17 02:40:34 +0800

    [script: new_file] 4.2.mt_cam_gallery.py

---
+++ script/4.make_cam_gallery/4.2.mt_cam_gallery.py
@@ -0,0 +1,16 @@
+import sys
+from pathlib import Path
+
+abs_module_path = Path("./../../").resolve()
+if (abs_module_path.exists()) and (str(abs_module_path) not in sys.path):
+    sys.path.append(str(abs_module_path)) # add path to scan customized module
+
+from modules.plot.cam_gallery.executor.mtcamgalleryexecutor import MtCamGalleryExecutor
+from modules.shared.utils import get_repo_root
+
+""" Detect Repository """
+print(f"Repository: '{get_repo_root()}'")
+
+
+mt_cam_gallery_executor = MtCamGalleryExecutor()
+mt_cam_gallery_executor.run()
