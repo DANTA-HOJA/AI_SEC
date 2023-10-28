14466199d8a73fa4341bf911d2280178f0530702
r10945018 <rime97410000@gmail.com>
2023-10-28 16:57:54 +0800

    [script: apply batch_config_mode] 2.1.vit_b_16.py

--- script/2.training/2.1.vit_b_16.py
+++ script/2.training/2.1.vit_b_16.py
@@ -6,6 +6,7 @@ if (abs_module_path.exists()) and (str(abs_module_path) not in sys.path):
     sys.path.append(str(abs_module_path)) # add path to scan customized module
 
 from modules.dl.trainer.vitb16trainer import VitB16Trainer
+from modules.shared.config import get_batch_config, get_batch_config_arg
 from modules.shared.utils import get_repo_root
 
 """ Detect Repository """
@@ -13,4 +14,12 @@ print(f"Repository: '{get_repo_root()}'")
 
 
 vit_b_16_trainer = VitB16Trainer()
-vit_b_16_trainer.run()
+args = get_batch_config_arg()
+
+if args.batch_mode == True:
+    
+    config_paths = sorted(get_batch_config(__file__))
+    for config_path in config_paths:
+        vit_b_16_trainer.run(config_path)
+
+else: vit_b_16_trainer.run()
