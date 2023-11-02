850b1199586328ca760328abc8cc160a03a7acd6
r10945018 <rime97410000@gmail.com>
2023-11-02 11:28:53 +0800

    [script: add fn] 2.1.vit_b_16.py

--- script/2.training/2.1.vit_b_16.py
+++ script/2.training/2.1.vit_b_16.py
@@ -7,7 +7,7 @@ if (abs_module_path.exists()) and (str(abs_module_path) not in sys.path):
 
 from modules.dl.trainer.vitb16trainer import VitB16Trainer
 from modules.shared.config import get_batch_config, get_batch_config_arg
-from modules.shared.utils import get_repo_root
+from modules.shared.utils import get_repo_root, exclude_tmp_paths
 
 """ Detect Repository """
 print(f"Repository: '{get_repo_root()}'")
@@ -18,7 +18,7 @@ args = get_batch_config_arg()
 
 if args.batch_mode == True:
     
-    config_paths = sorted(get_batch_config(__file__))
+    config_paths = sorted(exclude_tmp_paths(get_batch_config(__file__)))
     for config_path in config_paths:
         vit_b_16_trainer.run(config_path)
 
