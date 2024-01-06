bda9993d81f3f9c32672c0ae766c2a53440a41df
r10945018 <rime97410000@gmail.com>
2024-01-06 08:35:55 +0800

    [script: apply new fn] 0.1.check_lif_name.py

--- script/0.process_data/0.1.check_lif_name.py
+++ script/0.process_data/0.1.check_lif_name.py
@@ -6,11 +6,11 @@ if (abs_module_path.exists()) and (str(abs_module_path) not in sys.path):
     sys.path.append(str(abs_module_path)) # add path to scan customized module
 
 from modules.data.lif.batchlifnamechecker import BatchLIFNameChecker
-from modules.shared.utils import get_repo_root
+from modules.shared.utils import get_coupled_config_name, get_repo_root
+# -----------------------------------------------------------------------------/
 
 """ Detect Repository """
 print(f"Repository: '{get_repo_root()}'")
 
-
 batch_lif_name_checker = BatchLIFNameChecker()
-batch_lif_name_checker.run("0.1.check_lif_name.toml")
+batch_lif_name_checker.run(get_coupled_config_name(__file__))
