fd88848b6bbdb4eaa552a54342a4d5c9548eba8a
r10945018 <rime97410000@gmail.com>
2024-01-08 11:37:25 +0800

    [tool: substitute fn] test_ProcessedDataInstance.py

--- Tools/test_ProcessedDataInstance.py
+++ Tools/test_ProcessedDataInstance.py
@@ -13,38 +13,36 @@ abs_module_path = Path("./../").resolve()
 if (abs_module_path.exists()) and (str(abs_module_path) not in sys.path):
     sys.path.append(str(abs_module_path)) # add path to scan customized module
 
-from utils import get_tool_config_path
-
 from modules.data.processeddatainstance import ProcessedDataInstance
+from modules.shared.utils import get_coupled_config_name
 
 install()
 # -----------------------------------------------------------------------------/
 
-
 # init `ProcessedDataInstance`
 processed_data_instance = ProcessedDataInstance()
-processed_data_instance.set_attrs(get_tool_config_path(__file__))
+processed_data_instance.parse_config(get_coupled_config_name(__file__))
 
 # reminder
-temp_str = processed_data_instance.palmskin_processed_reminder
-print(f"palmskin_processed_reminder: '{temp_str}'")
 temp_str = processed_data_instance.brightfield_processed_reminder
-print(f"brightfield_processed_reminder: '{temp_str}'\n")
+print(f"brightfield_processed_reminder: '{temp_str}'")
+temp_str = processed_data_instance.palmskin_processed_reminder
+print(f"palmskin_processed_reminder: '{temp_str}'\n")
 
 # clustered_xlsx_files
-temp_dict = processed_data_instance.clustered_xlsx_files_dict
-print(">>> Clustered XLSX Files [ {x} files ]".format(x=len(temp_dict)))
+temp_dict = processed_data_instance.clustered_files_dict
+print(">>> Clustered Files [ {x} files ]".format(x=len(temp_dict)))
 print(Panel(Pretty(temp_dict, expand_all=True)), "\n")
 
 # alias_map
-temp_dict = processed_data_instance.brightfield_processed_alias_map
-print(">>> Brightfield Alias Map [ {x} files ]".format(x=len(temp_dict)))
+temp_dict = processed_data_instance.brightfield_processed_config
+print(">>> Brightfield Analyze Config: ")
 print(Panel(Pretty(temp_dict, expand_all=True)), "\n")
-temp_dict = processed_data_instance.palmskin_processed_alias_map
-print(">>> Palmskin Alias Map [ {x} files ]".format(x=len(temp_dict)))
+temp_dict = processed_data_instance.palmskin_processed_config
+print(">>> Palmskin Preprocess Config: ")
 print(Panel(Pretty(temp_dict, expand_all=True)), "\n")
 
 # target results
-rel_path, result_paths = processed_data_instance._get_sorted_results("palmskin", "RGB_fusion")
+rel_path, result_paths = processed_data_instance.get_sorted_results("palmskin", "31_RGB_fusion.tif")
 print(">>> Relative Path to `dname_dir`: {} [ found {} items ]".format(rel_path, len(result_paths)))
 print(Panel(Pretty(result_paths, expand_all=True)), "\n")
