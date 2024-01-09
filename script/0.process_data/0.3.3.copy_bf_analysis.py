44072f031fbca7e28c4b428f68453975e8c8e673
r10945018 <rime97410000@gmail.com>
2024-01-09 12:37:42 +0800

    [fn_set: upgrade] 0.3.3.copy_bf_analysis

--- script/0.process_data/0.3.3.copy_bf_analysis.py
+++ script/0.process_data/0.3.3.copy_bf_analysis.py
@@ -1,51 +1,73 @@
+import shutil
 import sys
 from pathlib import Path
-from colorama import Fore, Back, Style
-import shutil
 
-abs_module_path = Path("./../../../").resolve()
+from rich import print
+from rich.progress import *
+
+abs_module_path = Path("./../../").resolve()
 if (abs_module_path.exists()) and (str(abs_module_path) not in sys.path):
     sys.path.append(str(abs_module_path)) # add path to scan customized module
 
 from modules.data.processeddatainstance import ProcessedDataInstance
 from modules.shared.clioutput import CLIOutput
-from modules.shared.utils import get_repo_root
+from modules.shared.config import load_config
+from modules.shared.utils import get_coupled_config_name, get_repo_root
+# -----------------------------------------------------------------------------/
 
 """ Detect Repository """
 print(f"Repository: '{get_repo_root()}'")
 
-""" CLI output """
+# set variables
 cli_out = CLIOutput()
+cli_out._set_logger("Copy Brightfield Analysis")
+config = load_config(get_coupled_config_name(__file__))
+old_ins = config["instance_desc"]["old"]
+new_ins = config["instance_desc"]["new"]
+filenames = config["copy_info"]["filenames"]
 
 """ Old Instance ( with manual analysis results ) """
-cli_out.divide()
-old_instance = ProcessedDataInstance()
-old_instance.set_attrs(Path("old_instance.toml").resolve())
-old_dict = old_instance.brightfield_processed_dname_dirs_dict
+old_di = ProcessedDataInstance()
+old_di._cli_out._set_logger("ProcessedDataInstance (OLD)")
+old_di.parse_config({"data_processed": {"instance_desc": old_ins}})
+old_dict = old_di.brightfield_processed_dname_dirs_dict
 
 """ New Instance """
-cli_out.divide()
-new_instance = ProcessedDataInstance()
-new_instance.set_attrs(Path("new_instance.toml").resolve())
-new_dict = new_instance.brightfield_processed_dname_dirs_dict
+new_di = ProcessedDataInstance()
+new_di._cli_out._set_logger("ProcessedDataInstance (NEW)")
+new_di.parse_config({"data_processed": {"instance_desc": new_ins}})
+new_dict = new_di.brightfield_processed_dname_dirs_dict
 
 
 """ Main Process """
-cli_out.divide()
-filenames = ["Manual_measured_mask.tif", "ManualAnalysis.csv", "Manual_cropped_BF--MIX.tif"]
+progress = Progress(
+    SpinnerColumn(),
+    *Progress.get_default_columns(),
+    TextColumn("{task.completed} of {task.total}")
+)
 
-for dname, old_dir in old_dict.items():
+cli_out.divide()
+with progress:
     
-    try:
-        new_dir = new_dict[dname]
-        for filename in filenames:
-            
-            old_path = old_dir.joinpath(filename)
-            new_path = new_dir.joinpath(filename)
-            
-            if old_path.exists():
-                shutil.copy(old_path, new_path)
-                print(f"{Fore.BLUE}'{old_path}'\n -> {Fore.YELLOW}'{new_path}'{Style.RESET_ALL}\n")
+    task_desc = f"[yellow]{cli_out.logger_name}..."
+    task = progress.add_task(task_desc, total=len(old_dict))
+    
+    for dname, old_dir in old_dict.items():
         
-    except KeyError:
-        pass
+        try:
+            new_dir = new_dict[dname]
+            for filename in filenames:
+                
+                old_path = old_dir.joinpath(filename)
+                new_path = new_dir.joinpath(filename)
+                
+                if old_path.exists():
+                    shutil.copy(old_path, new_path)
+                    print(f"[#2596be]'{old_path}'\n [#FFFFFF]--> [#be4d25]'{new_path}'\n")
+            
+            progress.update(task, advance=1)
+            
+        except KeyError:
+            pass
+
+cli_out.new_line()
