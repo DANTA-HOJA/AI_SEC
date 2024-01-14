d3362970e606dbdb1251ab562a43470e2c45cbe4
r10945018 <rime97410000@gmail.com>
2024-01-15 07:41:59 +0800

    [fn: add cond] get_dsname_sortinfo()

--- modules/data/dataset/dsname.py
+++ modules/data/dataset/dsname.py
@@ -14,17 +14,23 @@ def get_dsname_sortinfo(string_with_fish_dsname:Union[str, Path]) -> tuple:
     elif isinstance(string_with_fish_dsname, str): pass
     else: raise TypeError("Unrecognized type of `string_with_fish_dsname`. Only `pathlib.Path` or `str` are accepted.")
     
-    file_name = string_with_fish_dsname.split(os.sep)[-1]
-    fish_dsname = file_name.split(".")[0] # [fish_dsname, tiff]
+    fish_dsname = string_with_fish_dsname.split(os.sep)[-1]
+    fish_dsname = os.path.splitext(fish_dsname)[0] # [fish_dsname, tiff]
     fish_dsname_split = re.split(" |_|-", fish_dsname)
     
-    if len(fish_dsname_split) == 4:
+    if len(fish_dsname_split) == 3:
+        """ fish_228_A  -->  ['fish', '228', 'A'] """
+        return int(fish_dsname_split[1]), fish_dsname_split[2]
+    elif len(fish_dsname_split) == 4:
         """ fish_228_A_D  -->  ['fish', '228', 'A', 'D'] """
         return int(fish_dsname_split[1]), fish_dsname_split[2], fish_dsname_split[3]
+    elif len(fish_dsname_split) == 5:
+        """ fish_228_A_D  -->  ['fish', '13', 'A', 'crop', '00'] """
+        return int(fish_dsname_split[1]), fish_dsname_split[2], int(fish_dsname_split[4])
     elif len(fish_dsname_split) == 6:
-        """ fish_228_A_D_crop_0  -->  ['fish', '228', 'A', 'D', 'crop', '0'] """
-        return int(fish_dsname_split[1]), fish_dsname_split[2], \
-                   fish_dsname_split[3], int(fish_dsname_split[5])
+        """ fish_228_A_D_crop_0  -->  ['fish', '228', 'A', 'D', 'crop', '00'] """
+        return int(fish_dsname_split[1]), fish_dsname_split[2], fish_dsname_split[3], \
+               int(fish_dsname_split[5])
     else:
         raise NotImplementedError(f"Unrecognized format of 'dsname' : {fish_dsname}")
     # -------------------------------------------------------------------------/
