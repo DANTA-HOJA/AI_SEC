ddd232706eb78b0502ca121a433f8b2a979cf7b5
r10945018 <rime97410000@gmail.com>
2024-01-16 02:29:11 +0800

    [fn_set: rename, upgrade] DatasetFileCreator

--- script/1.make_dataset/1.2.create_dataset_file.py
+++ script/1.make_dataset/1.2.create_dataset_file.py
@@ -5,12 +5,12 @@ abs_module_path = Path("./../../").resolve()
 if (abs_module_path.exists()) and (str(abs_module_path) not in sys.path):
     sys.path.append(str(abs_module_path)) # add path to scan customized module
 
-from modules.data.dataset.datasetxlsxcreator import DatasetXLSXCreator
+from modules.data.dataset.datasetfilecreator import DatasetFileCreator
 from modules.shared.utils import get_repo_root
+# -----------------------------------------------------------------------------/
 
 """ Detect Repository """
 print(f"Repository: '{get_repo_root()}'")
 
-
-dataset_xlsx_creator = DatasetXLSXCreator()
-dataset_xlsx_creator.run()
+dataset_file_creator = DatasetFileCreator()
+dataset_file_creator.run("1.make_dataset.toml")
