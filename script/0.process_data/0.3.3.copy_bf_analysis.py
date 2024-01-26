cce4215aaee71a5f878997f72ffdafac9cc088a0
r10945018 <rime97410000@gmail.com>
2024-01-27 07:01:48 +0800

    [script: upgrade] 0.3.3.copy_bf_analysis.py

--- script/0.process_data/0.3.3.copy_bf_analysis.py
+++ script/0.process_data/0.3.3.copy_bf_analysis.py
@@ -1,3 +1,4 @@
+import os
 import shutil
 import sys
 from pathlib import Path
@@ -12,7 +13,8 @@ if (abs_module_path.exists()) and (str(abs_module_path) not in sys.path):
 from modules.data.processeddatainstance import ProcessedDataInstance
 from modules.shared.clioutput import CLIOutput
 from modules.shared.config import load_config
-from modules.shared.utils import get_coupled_config_name, get_repo_root
+from modules.shared.utils import (create_new_dir, get_coupled_config_name,
+                                  get_repo_root)
 # -----------------------------------------------------------------------------/
 
 """ Detect Repository """
@@ -22,22 +24,22 @@ print(f"Repository: '{get_repo_root()}'")
 cli_out = CLIOutput()
 cli_out._set_logger("Copy Brightfield Analysis")
 config = load_config(get_coupled_config_name(__file__))
-old_ins = config["instance_desc"]["old"]
-new_ins = config["instance_desc"]["new"]
-filenames = config["copy_info"]["filenames"]
+old_ins: str = config["instance_desc"]["old"]
+new_ins: str = config["instance_desc"]["new"]
+image_type: str = config["copy_info"]["image_type"]
+result_names: list[str] = config["copy_info"]["result_name"]
 
 """ Old Instance ( with manual analysis results ) """
 old_di = ProcessedDataInstance()
 old_di._cli_out._set_logger("ProcessedDataInstance (OLD)")
 old_di.parse_config({"data_processed": {"instance_desc": old_ins}})
-old_dict = old_di.brightfield_processed_dname_dirs_dict
+old_processed_dir: Path = getattr(old_di, f"{image_type}_processed_dir")
 
 """ New Instance """
 new_di = ProcessedDataInstance()
 new_di._cli_out._set_logger("ProcessedDataInstance (NEW)")
 new_di.parse_config({"data_processed": {"instance_desc": new_ins}})
-new_dict = new_di.brightfield_processed_dname_dirs_dict
-
+new_processed_dir: Path = getattr(new_di, f"{image_type}_processed_dir")
 
 """ Main Process """
 progress = Progress(
@@ -49,27 +51,50 @@ progress = Progress(
 
 cli_out.divide()
 with progress:
-    
-    task_desc = f"[yellow]{cli_out.logger_name}..."
-    task = progress.add_task(task_desc, total=len(old_dict))
-    
-    for dname, old_dir in old_dict.items():
+
+    skipped_result: str = ""
+    for result_name in result_names:
         
-        try:
-            new_dir = new_dict[dname]
-            for filename in filenames:
-                
-                old_path = old_dir.joinpath(filename)
-                new_path = new_dir.joinpath(filename)
-                
-                if old_path.exists():
-                    shutil.copy(old_path, new_path)
-                    print(f"[#2596be]'{old_path}'\n [#FFFFFF]--> [#be4d25]'{new_path}'\n")
+        # >>> old target results <<<
+        rel_path, old_dict = \
+            old_di.get_sorted_results_dict(image_type, result_name,
+                                           allow_glob_dir=True)
+        if len(old_dict) == 0:
+            skipped_result += f"[magenta]Can't find any '{result_name}'\n"
+            continue
+        
+        # >>> new target results <<<
+        _, new_dict = \
+            new_di.get_sorted_results_dict(image_type, result_name,
+                                           allow_glob_dir=True)
+        
+        task_desc = f"[yellow]{cli_out.logger_name}: '{result_name}'..."
+        task = progress.add_task(task_desc, total=len(old_dict))
+        
+        for fish_dname, old_path in old_dict.items():
             
+            try:
+                # >>> `new_di` has target file? <<<
+                new_dict.pop(fish_dname)
+                
+            except KeyError:
+                
+                dname_dir = new_processed_dir.joinpath(fish_dname)
+                if dname_dir.exists():
+                    # >>> fish (dname) exists <<<
+                    new_path = dname_dir.joinpath(rel_path)
+                    
+                    if old_path.is_dir():
+                        shutil.copytree(old_path, new_path)
+                        
+                    elif old_path.is_file():
+                        create_new_dir(os.path.split(new_path)[0])
+                        shutil.copy(old_path, new_path)
+                    
+                    print(f"[#2596be]'{old_path}'\n [#FFFFFF]--> [#be4d25]'{new_path}'\n")
+                
             progress.update(task, advance=1)
             progress.refresh()
-            
-        except KeyError:
-            pass
 
-cli_out.new_line()
+cli_out.divide()
+print(skipped_result)
