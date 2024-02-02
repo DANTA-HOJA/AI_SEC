2a204a3b88a69f3adfc848f6075e9255ec20c60b
DANTA-HOJA <rime97410000@gmail.com>
2024-02-02 21:59:11 +0000

    [script: mv, rename] 3.a.vit_b_16.py (test_by_fish)

--- script/3.test_by_img/3.a.vit_b_16.py
+++ script/3.test_by_img/3.a.vit_b_16.py
@@ -1,9 +1,9 @@
 import sys
 from pathlib import Path
 
-abs_module_path = Path("./../../").resolve()
-if (abs_module_path.exists()) and (str(abs_module_path) not in sys.path):
-    sys.path.append(str(abs_module_path)) # add path to scan customized module
+pkg_dir = Path(__file__).parents[2] # `dir_depth` to `repo_root`
+if (pkg_dir.exists()) and (str(pkg_dir) not in sys.path):
+    sys.path.insert(0, str(pkg_dir)) # add path to scan customized package
 
 from modules.dl.tester.imagetester.vitb16imagetester import VitB16ImageTester
 from modules.shared.utils import get_repo_root
@@ -13,4 +13,4 @@ from modules.shared.utils import get_repo_root
 print(f"Repository: '{get_repo_root()}'")
 
 vit_b_16_image_tester = VitB16ImageTester()
-vit_b_16_image_tester.run("3.1.test_by_image.toml")
+vit_b_16_image_tester.run("3.test_by_image.toml")
