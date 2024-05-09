767b14c612da60006a939ff36f06fd67cbcbfd2c
DANTA-HOJA <rime97410000@gmail.com>
2024-05-10 07:10:11 +0800

    [fn_set: new] VitB16AOnlyTrainer

---
+++ script/2.training/2.e.aonly_vit_b_16.py
@@ -0,0 +1,27 @@
+import sys
+from pathlib import Path
+
+pkg_dir = Path(__file__).parents[2] # `dir_depth` to `repo_root`
+if (pkg_dir.exists()) and (str(pkg_dir) not in sys.path):
+    sys.path.insert(0, str(pkg_dir)) # add path to scan customized package
+
+from modules.dl.trainer.vitb16aonlytrainer import VitB16AOnlyTrainer
+from modules.shared.config import get_batch_config, get_batch_config_arg
+from modules.shared.utils import exclude_tmp_paths, get_repo_root
+
+import matplotlib; matplotlib.use("agg")
+# -----------------------------------------------------------------------------/
+
+""" Detect Repository """
+print(f"Repository: '{get_repo_root()}'")
+
+vit_b_16_aonly_trainer = VitB16AOnlyTrainer()
+args = get_batch_config_arg()
+
+if args.batch_mode == True:
+    
+    config_paths = sorted(exclude_tmp_paths(get_batch_config(__file__)))
+    for config_path in config_paths:
+        vit_b_16_aonly_trainer.run(config_path)
+
+else: vit_b_16_aonly_trainer.run("2.training.toml")
