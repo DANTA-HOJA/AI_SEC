e4e33be866cf0ee77ecc1b4d642a8746862ba7f1
DANTA-HOJA <rime97410000@gmail.com>
2024-05-10 07:10:38 +0800

    [fn_set: new] VitB16AOnlyImageTester

---
+++ modules/dl/tester/imagetester/vitb16aonlyimagetester.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import torch
+import torchvision
+from torch import nn
+
+from ...dataset.imgdataset import ImgDataset_v3
+from .vitb16imagetester import VitB16ImageTester
+# -----------------------------------------------------------------------------/
+
+
+class VitB16AOnlyImageTester(VitB16ImageTester):
+
+    def __init__(self, display_on_CLI=True) -> None:
+        """
+        """
+        # ---------------------------------------------------------------------
+        # """ components """
+        
+        super(VitB16ImageTester, self).__init__(display_on_CLI)
+        self._cli_out._set_logger("Vit_B_16 AOnly Image Tester")
+        
+        # ---------------------------------------------------------------------
+        # """ attributes """
+        # TODO
+        # ---------------------------------------------------------------------
+        # """ actions """
+        # TODO
+        # ---------------------------------------------------------------------/
+
+
+    def _set_test_df(self):
+        """
+        """
+        self.test_df: pd.DataFrame = \
+                self.dataset_df[(self.dataset_df["dataset"] == "test") &
+                                    (self.dataset_df["state"] == "preserve") &
+                                        (self.dataset_df["fish_pos"] == "A")]
+        
+        # if not self.add_bg_class:
+        #     self.test_df = self.test_df[(self.test_df["state"] == "preserve")]
+        
+        # debug: sampleing for faster speed
+        if self.debug_mode:
+            self.test_df = self.test_df.sample(n=self.debug_rand_select,
+                                               replace=False,
+                                               random_state=self.rand_seed)
+            self._cli_out.write(f"※　: debug mode, reduce to only {self.debug_rand_select} images")
+        # ---------------------------------------------------------------------/
