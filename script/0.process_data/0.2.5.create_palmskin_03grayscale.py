45d21af40af2a322306d1ed2c552c1f037842dd7
DANTA-HOJA <rime97410000@gmail.com>
2024-06-07 03:45:34 +0800

    [script: mv, rename, optimize] 0.2.5.create_palmskin_03grayscale.py

---
+++ script/0.process_data/0.2.5.create_palmskin_03grayscale.py
@@ -0,0 +1,48 @@
+import sys
+from pathlib import Path
+
+import cv2
+from rich import print
+from rich.console import Console
+from rich.pretty import Pretty
+from rich.traceback import install
+
+pkg_dir = Path(__file__).parents[2] # `dir_depth` to `repo_root`
+if (pkg_dir.exists()) and (str(pkg_dir) not in sys.path):
+    sys.path.insert(0, str(pkg_dir)) # add path to scan customized package
+
+from modules.data.processeddatainstance import ProcessedDataInstance
+from modules.shared.clioutput import CLIOutput
+from modules.shared.utils import get_repo_root
+
+install()
+# -----------------------------------------------------------------------------/
+
+
+if __name__ == '__main__':
+
+    print(f"Repository: '{get_repo_root()}'")
+
+    """ Init components """
+    console = Console()
+    cli_out = CLIOutput()
+    processed_di = ProcessedDataInstance()
+
+    processed_di.parse_config("0.2.3.process_palmskin_manualroi.toml")
+    print("", Pretty(processed_di.config, expand_all=True))
+    cli_out.divide()
+
+    with console.status("running..."):
+        for path in processed_di.palmskin_processed_dname_dirs_dict.values():
+            
+            # target (dst path)
+            target = path.joinpath("03_RGB_direct_max_zproj2Gray.tif")
+            if not target.exists():
+                
+                # read source image (03 color)
+                source = path.joinpath("03_RGB_direct_max_zproj.tif")
+                img = cv2.imread(str(source))
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                
+                cv2.imwrite(str(target), img)
+                print(f"[ {target.parent.stem} : '{target}' ]")
