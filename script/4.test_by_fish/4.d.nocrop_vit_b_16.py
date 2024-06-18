466ea6734db84e3d7117b2e96750bba143cf19ff
DANTA-HOJA <rime97410000@gmail.com>
2024-06-18 15:18:01 +0800

    [fn_set: new] VitB16NoCropFishTester

---
+++ script/4.test_by_fish/4.d.nocrop_vit_b_16.py
@@ -0,0 +1,20 @@
+import sys
+from pathlib import Path
+
+from rich.traceback import install
+
+pkg_dir = Path(__file__).parents[2] # `dir_depth` to `repo_root`
+if (pkg_dir.exists()) and (str(pkg_dir) not in sys.path):
+    sys.path.insert(0, str(pkg_dir)) # add path to scan customized package
+
+from modules.dl.tester.fishtester.vitb16nocropfishtester import VitB16NoCropFishTester
+from modules.shared.utils import get_repo_root
+
+install()
+# -----------------------------------------------------------------------------/
+
+""" Detect Repository """
+print(f"Repository: '{get_repo_root()}'")
+
+vit_b_16_nocrop_fish_tester = VitB16NoCropFishTester()
+vit_b_16_nocrop_fish_tester.run("4.test_by_fish.toml")
