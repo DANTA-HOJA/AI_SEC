92b32cf80020aea4670543017b75573fc7ed8131
DANTA-HOJA <rime97410000@gmail.com>
2024-10-02 16:41:57 +0800

    [fn_set: new] resnet50_nocrop_trainer

---
+++ modules/dl/trainer/resnet50nocroptrainer.py
@@ -0,0 +1,64 @@
+import torchvision
+from imgaug import augmenters as iaa
+from torch import nn, optim
+
+from ..dataset.augmentation import composite_aug
+from ..dataset.imgdataset import NoCropImgDataset_v3
+from .resnet50trainer import ResNet50Trainer
+from .utils import calculate_class_weight
+# -----------------------------------------------------------------------------/
+
+
+class ResNet50NoCropTrainer(ResNet50Trainer):
+
+    def __init__(self, display_on_CLI=True) -> None:
+        """
+        """
+        # ---------------------------------------------------------------------
+        # """ components """
+        
+        super(ResNet50Trainer, self).__init__(display_on_CLI=display_on_CLI)
+        self._cli_out._set_logger("ResNet50 NoCrop Trainer")
+        
+        # ---------------------------------------------------------------------
+        # """ attributes """
+        # TODO
+        # ---------------------------------------------------------------------
+        # """ actions """
+        # TODO
+        # ---------------------------------------------------------------------/
+
+
+    def _set_train_set(self):
+        """
+        """
+        resize: int = 224
+        intensity_thres: int = 30
+        
+        if self.aug_on_fly is True: 
+            transform: iaa.Sequential = composite_aug()
+        else:
+            raise ValueError("Detect error settings in config: "
+                             f"train_opts.data.aug_on_fly = {self.aug_on_fly}")
+            transform = None
+        
+        self.train_set: NoCropImgDataset_v3 = \
+            NoCropImgDataset_v3("train", self.config, self.train_df,
+                                self.class2num_dict, resize, intensity_thres,
+                                transform=transform, dst_root=self.dst_root,
+                                debug_mode=self.debug_mode, display_on_CLI=True)
+        # ---------------------------------------------------------------------/
+
+
+    def _set_valid_set(self):
+        """
+        """
+        resize: int = 224
+        intensity_thres: int = 30
+        
+        self.valid_set: NoCropImgDataset_v3 = \
+            NoCropImgDataset_v3("valid", self.config, self.valid_df,
+                                self.class2num_dict, resize, intensity_thres,
+                                transform=None, dst_root=self.dst_root,
+                                debug_mode=self.debug_mode, display_on_CLI=True)
+        # ---------------------------------------------------------------------/
---
+++ script/2.training/2.k.nocrop_resnet_50.py
@@ -0,0 +1,27 @@
+import sys
+from pathlib import Path
+
+pkg_dir = Path(__file__).parents[2] # `dir_depth` to `repo_root`
+if (pkg_dir.exists()) and (str(pkg_dir) not in sys.path):
+    sys.path.insert(0, str(pkg_dir)) # add path to scan customized package
+
+from modules.dl.trainer.resnet50nocroptrainer import ResNet50NoCropTrainer
+from modules.shared.config import get_batch_config, get_batch_config_arg
+from modules.shared.utils import exclude_tmp_paths, get_repo_root
+
+import matplotlib; matplotlib.use("agg")
+# -----------------------------------------------------------------------------/
+
+""" Detect Repository """
+print(f"Repository: '{get_repo_root()}'")
+
+resnet50_nocrop_trainer = ResNet50NoCropTrainer()
+args = get_batch_config_arg()
+
+if args.batch_mode == True:
+    
+    config_paths = sorted(exclude_tmp_paths(get_batch_config(__file__)))
+    for config_path in config_paths:
+        resnet50_nocrop_trainer.run(config_path)
+
+else: resnet50_nocrop_trainer.run("2.training.toml")
