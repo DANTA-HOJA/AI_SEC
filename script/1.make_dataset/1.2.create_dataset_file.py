4cd856633f5e2bd97a6a07aaa060797b35411bcc
DANTA-HOJA <rime97410000@gmail.com>
2024-10-09 06:12:33 +0800

    [script: support `batch_mode`] 1.2.create_dataset_file.py

--- script/1.make_dataset/1.2.create_dataset_file.py
+++ script/1.make_dataset/1.2.create_dataset_file.py
@@ -6,11 +6,20 @@ if (pkg_dir.exists()) and (str(pkg_dir) not in sys.path):
     sys.path.insert(0, str(pkg_dir)) # add path to scan customized package
 
 from modules.data.dataset.datasetfilecreator import DatasetFileCreator
-from modules.shared.utils import get_repo_root
+from modules.shared.config import get_batch_config, get_batch_config_arg
+from modules.shared.utils import exclude_tmp_paths, get_repo_root
 # -----------------------------------------------------------------------------/
 
 """ Detect Repository """
 print(f"Repository: '{get_repo_root()}'")
 
 dataset_file_creator = DatasetFileCreator()
-dataset_file_creator.run("1.make_dataset.toml")
+args = get_batch_config_arg()
+
+if args.batch_mode == True:
+    
+    config_paths = sorted(exclude_tmp_paths(get_batch_config(__file__)))
+    for config_path in config_paths:
+        dataset_file_creator.run(config_path)
+
+else: dataset_file_creator.run("1.make_dataset.toml")
