fcc30fe93f62540016efc9781e02c5bea7af51ee
DANTA-HOJA <rime97410000@gmail.com>
2024-10-23 07:02:25 +0800

    [fn_set: new] VitB16NormBFImageTester

---
+++ script/3.test_by_img/3.l.normbf_vit_b_16.py
@@ -0,0 +1,25 @@
+import sys
+from pathlib import Path
+
+pkg_dir = Path(__file__).parents[2] # `dir_depth` to `repo_root`
+if (pkg_dir.exists()) and (str(pkg_dir) not in sys.path):
+    sys.path.insert(0, str(pkg_dir)) # add path to scan customized package
+
+from modules.dl.tester.imagetester.vitb16normbfimagetester import VitB16NormBFImageTester
+from modules.shared.config import get_batch_config, get_batch_config_arg
+from modules.shared.utils import exclude_tmp_paths, get_repo_root
+# -----------------------------------------------------------------------------/
+
+""" Detect Repository """
+print(f"Repository: '{get_repo_root()}'")
+
+vit_b_16_normbf_image_tester = VitB16NormBFImageTester()
+args = get_batch_config_arg()
+
+if args.batch_mode == True:
+    
+    config_paths = sorted(exclude_tmp_paths(get_batch_config(__file__)))
+    for config_path in config_paths:
+        vit_b_16_normbf_image_tester.run(config_path)
+
+else: vit_b_16_normbf_image_tester.run("3.test_by_image.toml")
