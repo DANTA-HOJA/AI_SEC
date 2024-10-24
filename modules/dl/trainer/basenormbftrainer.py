3892aeed91222f98f73e15757973c1ed7e344bd3
DANTA-HOJA <rime97410000@gmail.com>
2024-10-24 23:53:59 +0800

    [class: bugfix] BaseNormBFTrainer

--- modules/dl/trainer/basenormbftrainer.py
+++ modules/dl/trainer/basenormbftrainer.py
@@ -95,7 +95,7 @@ class BaseNormBFTrainer(BaseObject):
         self.dataset_palmskin_result: str = self.config["dataset"]["palmskin_result"]
         assert self.config["dataset"]["base_size"] == "", "`dataset.base_size` in config should be empty (\"\")"
         self.dataset_classif_strategy: str = self.config["dataset"]["classif_strategy"]
-        assert self.config["dataset"]["file_name"] == "", "`dataset.file_name` in config should be `DS_SURF3C_NORMBF.xxx`"
+        assert self.config["dataset"]["file_name"] == "DS_SURF3C_NORMBF.xxx", "`dataset.file_name` in config should be `DS_SURF3C_NORMBF.xxx`"
         
         """ [model] """
         self.model_name: str = self.config["model"]["name"]
