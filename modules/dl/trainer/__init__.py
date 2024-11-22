7d9b2671304959a9b920079dd6481505a66b4c66
DANTA-HOJA <rime97410000@gmail.com>
2024-11-22 13:59:44 +0800

    [module: import class from '__init__.py'] dl.trainer

---
+++ modules/dl/trainer/__init__.py
@@ -0,0 +1,18 @@
+from .basetrainer import BaseTrainer
+from .basesurfdgttrainer import BaseSurfDGTTrainer
+
+from .vitb16trainer import VitB16Trainer
+from .vitb16aonlytrainer import VitB16AOnlyTrainer
+from .vitb16ponlytrainer import VitB16POnlyTrainer
+
+from .vitb16nocroptrainer import VitB16NoCropTrainer
+from .vitb16aonlynocroptrainer import VitB16AOnlyNoCropTrainer
+from vitb16ponlynocroptrainer import VitB16POnlyNoCropTrainer
+
+from .vitb16surfdgttrainer import VitB16SurfDGTTrainer
+from .vitb16aonlysurfdgttrainer import VitB16AOnlySurfDGTTrainer
+from .vitb16ponlysurfdgttrainer import VitB16POnlySurfDGTTrainer
+
+from .resnet50trainer import ResNet50Trainer
+
+from .resnet50nocroptrainer import ResNet50NoCropTrainer
