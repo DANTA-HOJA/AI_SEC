04421d81fd523fc1bb538ab0e347ddb4c67667e3
DANTA-HOJA <rime97410000@gmail.com>
2024-12-26 06:05:40 +0800

    [upgrad: add cellpose mode] 2.create_ml_dataset.py

--- script_ml/2.create_ml_dataset.py
+++ script_ml/2.create_ml_dataset.py
@@ -14,9 +14,11 @@ if (pkg_dir.exists()) and (str(pkg_dir) not in sys.path):
 
 from modules.data import dname
 from modules.data.processeddatainstance import ProcessedDataInstance
-from modules.ml.utils import get_slic_param_name
+from modules.ml.utils import (get_cellpose_param_name, get_seg_desc,
+                              get_slic_param_name)
 from modules.shared.clioutput import CLIOutput
 from modules.shared.config import load_config
+from modules.shared.pathnavigator import PathNavigator
 from modules.shared.utils import create_new_dir, get_repo_root
 
 install()
@@ -30,6 +32,7 @@ if __name__ == '__main__':
     """ Init components """
     cli_out = CLIOutput()
     cli_out.divide()
+    path_navigator = PathNavigator()
     processed_di = ProcessedDataInstance()
     processed_di.parse_config("ml_analysis.toml")
 
@@ -38,14 +41,27 @@ if __name__ == '__main__':
     # [data_processed]
     palmskin_result_name: Path = Path(config["data_processed"]["palmskin_result_name"])
     cluster_desc: str = config["data_processed"]["cluster_desc"]
+    # [seg_results]
+    seg_desc = get_seg_desc(config)
+    # [Cellpose]
+    cp_model_name: str = config["Cellpose"]["cp_model_name"]
     # [ML]
     topn_patch = config["ML"]["topn_patch"]
     print("", Pretty(config, expand_all=True))
     cli_out.divide()
     
-    # get `slic_dirname`
-    slic_param_name = get_slic_param_name(config)
-    slic_dirname = f"{palmskin_result_name.stem}.{slic_param_name}"
+    # get `seg_dirname`
+    if seg_desc == "SLIC":
+        seg_param_name = get_slic_param_name(config)
+    elif seg_desc == "Cellpose":
+        # check model
+        cp_model_dir = path_navigator.dbpp.get_one_of_dbpp_roots("model_cellpose")
+        cp_model_path = cp_model_dir.joinpath(cp_model_name)
+        if cp_model_path.is_file():
+            seg_param_name = get_cellpose_param_name(config)
+        else:
+            raise FileNotFoundError(f"'{cp_model_path}' is not a file or does not exist")
+    seg_dirname = f"{palmskin_result_name.stem}.{seg_param_name}"
     
     # load `clustered file`
     csv_path = processed_di.clustered_files_dict[cluster_desc]
@@ -60,9 +76,9 @@ if __name__ == '__main__':
     for palmskin_dname in palmskin_dnames:
         # prepare
         path = processed_di.palmskin_processed_dir.joinpath(palmskin_dname)
-        slic_analysis = path.joinpath("SLIC", slic_dirname,
-                                      f"{slic_dirname}.ana.toml")
-        slic_analysis = load_config(slic_analysis)
+        seg_analysis = path.joinpath(seg_desc, seg_dirname,
+                                     f"{seg_dirname}.ana.toml")
+        seg_analysis = load_config(seg_analysis)
         fish_id = dname.get_dname_sortinfo(palmskin_dname)[0]
         
         # >>> Create `temp_dict` <<<
@@ -72,11 +88,11 @@ if __name__ == '__main__':
         temp_dict["class"] = clustered_df.loc[fish_id, "class"]
         temp_dict["dataset"] = clustered_df.loc[fish_id, "dataset"]
         # -------------------------------------------------------
-        for k, v in slic_analysis.items():
+        for k, v in seg_analysis.items():
             if k == f"patch_sizes": continue
             temp_dict[k] = v
         
-        for i, patch_size in enumerate(slic_analysis[f"patch_sizes"], start=1):
+        for i, patch_size in enumerate(seg_analysis[f"patch_sizes"], start=1):
             temp_dict[f"top{i}_patch"] = patch_size
             if i == topn_patch: break
         # -------------------------------------------------------
@@ -89,10 +105,10 @@ if __name__ == '__main__':
     dataset_df = dataset_df.dropna(axis=1)
     
     # save Dataframe as a CSV file
-    save_path = Path(__file__).parent.joinpath("data/generated/ML",
-                                               processed_di.instance_name,
-                                               cluster_desc, slic_dirname,
-                                               f"ml_dataset.csv")
+    result_ml_dir = path_navigator.dbpp.get_one_of_dbpp_roots("result_ml")
+    save_path = result_ml_dir.joinpath(processed_di.instance_name, cluster_desc,
+                                       seg_desc, seg_dirname,
+                                       f"ml_dataset.csv")
     create_new_dir(save_path.parent)
     dataset_df.to_csv(save_path, encoding='utf_8_sig', index=False)
     print(f"ML_table: '{save_path}'\n")
