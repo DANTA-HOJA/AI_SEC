37e72cfad957e1c6fa39da09f2507ff9024d4fbb
DANTA-HOJA <rime97410000@gmail.com>
2024-12-26 02:54:27 +0800

    [upgrade: add cellpose] 1.get_cell_feature.py

--- script_ml/1.get_cell_feature.py
+++ script_ml/1.get_cell_feature.py
@@ -18,10 +18,13 @@ from modules.ml.calc_seg_feat import (count_area, count_average_size,
                                       count_element, get_patch_sizes,
                                       update_ana_toml_file,
                                       update_seg_analysis_dict)
-from modules.ml.seg_generate import single_slic_labeling
-from modules.ml.utils import get_slic_param_name
+from modules.ml.seg_generate import (single_cellpose_prediction,
+                                     single_slic_labeling)
+from modules.ml.utils import (get_cellpose_param_name, get_seg_desc,
+                              get_slic_param_name)
 from modules.shared.clioutput import CLIOutput
 from modules.shared.config import load_config
+from modules.shared.pathnavigator import PathNavigator
 from modules.shared.utils import create_new_dir, get_repo_root
 
 install()
@@ -35,6 +38,7 @@ if __name__ == '__main__':
     """ Init components """
     cli_out = CLIOutput()
     cli_out.divide()
+    path_navigator = PathNavigator()
     processed_di = ProcessedDataInstance()
     processed_di.parse_config("ml_analysis.toml")
 
@@ -42,20 +46,43 @@ if __name__ == '__main__':
     config = load_config("ml_analysis.toml")
     # [data_processed]
     palmskin_result_name: Path = Path(config["data_processed"]["palmskin_result_name"])
+    # [seg_results]
+    seg_desc = get_seg_desc(config)
     # [SLIC]
     n_segments: int  = config["SLIC"]["n_segments"]
     dark: int        = config["SLIC"]["dark"]
-    merge: int       = config["SLIC"]["merge"]
-    debug_mode: bool = config["SLIC"]["debug_mode"]
+    # [Cellpose]
+    cp_model_name: str = config["Cellpose"]["cp_model_name"]
+    channels: list     = config["Cellpose"]["channels"]
     print("", Pretty(config, expand_all=True))
     cli_out.divide()
 
     # 'W512_H1024' cropper
     w512h1024_cropper = crop_base_size(512, 1024)
     
-    # get `slic_dirname`
-    slic_param_name = get_slic_param_name(config)
-    slic_dirname = f"{palmskin_result_name.stem}.{slic_param_name}"
+    # get `seg_dirname`
+    merge: int       = config[f"{seg_desc}"]["merge"]
+    debug_mode: bool = config[f"{seg_desc}"]["debug_mode"]
+    if seg_desc == "SLIC":
+        seg_param_name = get_slic_param_name(config)
+    elif seg_desc == "Cellpose":
+        # check model
+        cp_model_dir = path_navigator.dbpp.get_one_of_dbpp_roots("model_cellpose")
+        cp_model_path = cp_model_dir.joinpath(cp_model_name)
+        if cp_model_path.is_file():
+            seg_param_name = get_cellpose_param_name(config)
+        else:
+            raise FileNotFoundError(f"'{cp_model_path}' is not a file or does not exist")
+        # load model
+        if "cellpose" in sys.executable: # check python environment
+                from cellpose import models as cpmodels
+                cp_model = cpmodels.CellposeModel(gpu=True, pretrained_model=str(cp_model_path))
+        else:
+            raise RuntimeError("Detect environment name not for Cellpose. "
+                                "Please follow the setup instructions provided at "
+                                "'https://github.com/MouseLand/cellpose' "
+                                "to create an environment.")
+    seg_dirname = f"{palmskin_result_name.stem}.{seg_param_name}"
     
     """ Colloct image file names """
     rel_path, sorted_results_dict = \
@@ -73,20 +100,26 @@ if __name__ == '__main__':
             dname_dir = Path(str(result_path).replace(rel_path, ""))
             print(f"[ {dname_dir.parts[-1]} ]")
             
-            # create `slic_dir`
-            slic_dir = dname_dir.joinpath(f"SLIC/{slic_dirname}")
-            create_new_dir(slic_dir)
-            
             # get image, size: W512_H1024 (FixedROI)
-            target_path = slic_dir.parent.joinpath(f"{result_path.stem}.W512_H1024.tif")
+            target_path = dname_dir.joinpath(f"CenterCropped/{result_path.stem}.W512_H1024.tif")
             if not target_path.exists():
+                create_new_dir(target_path.parent)
                 tmp_img = w512h1024_cropper(image=cv2.imread(str(result_path)))
                 cv2.imwrite(str(target_path), tmp_img)
             
-            # slic
-            cell_seg, patch_seg = single_slic_labeling(slic_dir, target_path,
-                                                       n_segments, dark, merge,
-                                                       debug_mode)
+            # dname_dir
+            d_seg_dir = dname_dir.joinpath(f"{seg_desc}/{seg_dirname}")
+            create_new_dir(d_seg_dir)
+            
+            # generate cell segmentation
+            if seg_desc == "SLIC":
+                cell_seg, patch_seg = single_slic_labeling(d_seg_dir, target_path,
+                                                           n_segments, dark, merge,
+                                                           debug_mode)
+            elif seg_desc == "Cellpose":
+                cell_seg, patch_seg = single_cellpose_prediction(d_seg_dir, target_path,
+                                                                 channels, cp_model, merge,
+                                                                 debug_mode)
             
             # update
             analysis_dict = {}
@@ -99,7 +132,7 @@ if __name__ == '__main__':
             cli_out.new_line()
             
             # update info to toml file
-            ana_toml_file = slic_dir.joinpath(f"{slic_dirname}.ana.toml")
+            ana_toml_file = d_seg_dir.joinpath(f"{seg_dirname}.ana.toml")
             update_ana_toml_file(ana_toml_file, analysis_dict)
             
             # update pbar
