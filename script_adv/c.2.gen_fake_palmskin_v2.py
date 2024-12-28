3685586bf3b030d24af226a986a5012c7eb405bc
DANTA-HOJA <rime97410000@gmail.com>
2024-12-28 14:38:11 +0800

    [script: upgrade cellpose mode] c.2.gen_fake_palmskin_v2.py

--- script_adv/c.2.gen_fake_palmskin_v2.py
+++ script_adv/c.2.gen_fake_palmskin_v2.py
@@ -14,15 +14,15 @@ from rich.progress import Progress
 from rich.traceback import install
 from skimage.segmentation import mark_boundaries
 
-pkg_dir = Path(__file__).parents[2] # `dir_depth` to `repo_root`
+pkg_dir = Path(__file__).parents[1] # `dir_depth` to `repo_root`
 if (pkg_dir.exists()) and (str(pkg_dir) not in sys.path):
     sys.path.insert(0, str(pkg_dir)) # add path to scan customized package
 
-from modules.data.dataset.dsname import get_dsname_sortinfo
-from modules.data.dname import get_dname_sortinfo
 from modules.data.processeddatainstance import ProcessedDataInstance
 from modules.dl.fakepalmskin.utils import (gen_singlecolor_palmskin,
                                            gen_unique_random_color_pool)
+from modules.ml.utils import (get_cellpose_param_name, get_seg_desc,
+                              get_slic_param_name)
 from modules.shared.clioutput import CLIOutput
 from modules.shared.config import load_config
 from modules.shared.pathnavigator import PathNavigator
@@ -37,70 +37,53 @@ if __name__ == '__main__':
     print(f"Repository: '{get_repo_root()}'")
     
     """ Init components """
-    processed_di = ProcessedDataInstance()
-    path_navigator = PathNavigator()
     cli_out = CLIOutput()
     cli_out.divide()
+    path_navigator = PathNavigator()
+    processed_di = ProcessedDataInstance()
+    processed_di.parse_config("ml_analysis.toml")
 
-    # load config
-    # `dark` and `merge` are two parameters as color space distance, determined by experiences
-    config = load_config("get_cell_feature.toml")
-    # [dataset]
-    dataset_seed_dir: str = config["dataset"]["seed_dir"]
-    dataset_data: str = config["dataset"]["data"]
-    dataset_palmskin_result: str = config["dataset"]["palmskin_result"]
-    dataset_base_size: str = "W512_H1024"
-    # [SLIC]
-    n_segments: int  = config["SLIC"]["n_segments"]
-    dark: int        = config["SLIC"]["dark"]
-    merge: int       = config["SLIC"]["merge"]
-    debug_mode: bool = config["SLIC"]["debug_mode"]
+    """ Load config """
+    config = load_config("ml_analysis.toml")
+    # [data_processed]
+    palmskin_result_name: Path = Path(config["data_processed"]["palmskin_result_name"])
+    cluster_desc: str = config["data_processed"]["cluster_desc"]
+    # [seg_results]
+    seg_desc = get_seg_desc(config)
+    # [Cellpose]
+    cp_model_name: str = config["Cellpose"]["cp_model_name"]
     print("", Pretty(config, expand_all=True))
     cli_out.divide()
 
+    # get `seg_dirname`
+    if seg_desc == "SLIC":
+        seg_param_name = get_slic_param_name(config)
+    elif seg_desc == "Cellpose":
+        # check model
+        cp_model_dir = path_navigator.dbpp.get_one_of_dbpp_roots("model_cellpose")
+        cp_model_path = cp_model_dir.joinpath(cp_model_name)
+        if cp_model_path.is_file():
+            seg_param_name = get_cellpose_param_name(config)
+        else:
+            raise FileNotFoundError(f"'{cp_model_path}' is not a file or does not exist")
+    seg_dirname = f"{palmskin_result_name.stem}.{seg_param_name}"
+
     # set `random seed`
-    random.seed(int(dataset_seed_dir.replace("RND", "")))
-    
-    """ Colloct image file names """
-    dataset_cropped: Path = path_navigator.dbpp.get_one_of_dbpp_roots("dataset_cropped_v3")
-    src_root = dataset_cropped.joinpath(dataset_seed_dir,
-                                        dataset_data,
-                                        dataset_palmskin_result,
-                                        dataset_base_size)
-    paths = sorted(src_root.glob("*/*/*.tiff"), key=get_dsname_sortinfo)
-    print(f"Total files: {len(paths)}")
-    
-    """ Processed Data Instance """
-    instance_desc = re.split("{|}", dataset_data)[1]
-    temp_dict = {"data_processed": {"instance_desc": instance_desc}}
-    processed_di.parse_config(temp_dict)
-    csv_path = processed_di.instance_root.joinpath("data.csv")
-    df: pd.DataFrame = pd.read_csv(csv_path, encoding='utf_8_sig')
-    palmskin_dnames = sorted(pd.concat([df["Palmskin Anterior (SP8)"],
-                                        df["Palmskin Posterior (SP8)"]]),
-                            key=get_dname_sortinfo)
-    
+    random.seed(42)
+
     """ Apply SLIC on each image """
     cli_out.divide()
     with Progress() as pbar:
-        task = pbar.add_task("[cyan]Processing...", total=len(paths))
+        task = pbar.add_task("[cyan]Processing...",
+                             total=len(processed_di.palmskin_processed_dname_dirs_dict))
         
-        for path, dname in zip(paths, palmskin_dnames):
-            
-            # check dsname, dname is match
-            assert get_dsname_sortinfo(path) == get_dname_sortinfo(dname), "dsname, dname not match"
-            
-            result_name = path.stem
-            dname_dir = path.parents[0]
-            
-            # check analyze condiction is same
-            verify_cfg = dname_dir.joinpath(f"SLIC/{result_name}_{{dark_{dark}}}/{{copy}}_get_cell_feature.toml")
-            assert config == load_config(verify_cfg), f"`verify_cfg` not match, '{verify_cfg}'"
+        for dname_dir in processed_di.palmskin_processed_dname_dirs_dict.values():
             
             fake_imgs = {} # 4 types
+            src_dir = dname_dir.joinpath(seg_desc, seg_dirname)
 
             """Random color #2: load `cell_seg` (seg1, without clonal information)"""
-            seg1_pkl = dname_dir.joinpath(f"SLIC/{result_name}_{{dark_{dark}}}/{result_name}.seg1.pkl")
+            seg1_pkl = src_dir.joinpath(f"{seg_dirname}.seg1.pkl")
             print(f"[ {seg1_pkl.parts[-1]} : '{seg1_pkl}' ]")
             with open(seg1_pkl, mode="rb") as f_reader:
                 seg1 = pkl.load(f_reader)
@@ -127,7 +110,7 @@ if __name__ == '__main__':
                 mark_boundaries(colored_seg1, seg1, color=(1.0, 1.0, 1.0))
             
             """Random color #1: load `clone_seg` (seg2, with clonal information)"""
-            seg2_pkl = dname_dir.joinpath(f"SLIC/{result_name}_{{dark_{dark}}}/{result_name}.seg2.pkl")
+            seg2_pkl = src_dir.joinpath(f"{seg_dirname}.seg2.pkl")
             print(f"[ {seg2_pkl.parts[-1]} : '{seg2_pkl}' ]")
             with open(seg2_pkl, mode="rb") as f_reader:
                 seg2 = pkl.load(f_reader)
@@ -161,11 +144,10 @@ if __name__ == '__main__':
                                          border_color=0.5, bg_color=0.0)
             
             """Save images"""
-            dpath = processed_di.palmskin_processed_dname_dirs_dict[dname]
-            fakeimg_dir = dpath.joinpath(f"FakeImage_v2/{dataset_palmskin_result}_{{dark_{dark}}}")
+            fakeimg_dir = dname_dir.joinpath(f"FakeImage_v2", seg_desc, seg_dirname)
             create_new_dir(fakeimg_dir)
             for key, img in fake_imgs.items():
-                save_path = fakeimg_dir.joinpath(f"{dataset_palmskin_result}.{key}.tif")
+                save_path = fakeimg_dir.joinpath(f"{seg_dirname}.{key}.tif")
                 assert img.dtype == np.float64 # make sure image pixels are present in float
                 ski.io.imsave(save_path, np.uint8(img*255)) # float to 8-bit image
                 print(f"{key} : '{save_path}'")
