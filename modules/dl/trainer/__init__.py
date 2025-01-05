bc45066902c289ad1b0bbb1612c1ee0b9c348a8f
DANTA-HOJA <rime97410000@gmail.com>
2025-01-05 19:52:57 +0800

    [module: add `NormBF` class] modules.dl.trainer.__init__

--- modules/dl/trainer/__init__.py
+++ modules/dl/trainer/__init__.py
@@ -1,5 +1,6 @@
 from .basetrainer import BaseTrainer
 from .basesurfdgttrainer import BaseSurfDGTTrainer
+from .basenormbftrainer import BaseNormBFTrainer
 
 from .vitb16trainer import VitB16Trainer
 from .vitb16aonlytrainer import VitB16AOnlyTrainer
@@ -12,6 +13,8 @@ from .vitb16ponlynocroptrainer import VitB16POnlyNoCropTrainer
 from .vitb16surfdgttrainer import VitB16SurfDGTTrainer
 from .vitb16aonlysurfdgttrainer import VitB16AOnlySurfDGTTrainer
 from .vitb16ponlysurfdgttrainer import VitB16POnlySurfDGTTrainer
+
+from .vitb16normbftrainer import VitB16NormBFTrainer
 
 from .resnet50trainer import ResNet50Trainer
 
