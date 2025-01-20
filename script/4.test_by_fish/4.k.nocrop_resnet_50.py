548e0bb77219ecf01cea47bccbe9579b6e2457fb
DANTA-HOJA <rime97410000@gmail.com>
2025-01-20 12:15:09 +0800

    [script: add `batch_mode`] (DL) 3.k and 4.k

--- script/4.test_by_fish/4.k.nocrop_resnet_50.py
+++ script/4.test_by_fish/4.k.nocrop_resnet_50.py
@@ -8,7 +8,8 @@ if (pkg_dir.exists()) and (str(pkg_dir) not in sys.path):
     sys.path.insert(0, str(pkg_dir)) # add path to scan customized package
 
 from modules.dl.tester.fishtester import ResNet50NoCropFishTester
-from modules.shared.utils import get_repo_root
+from modules.shared.config import get_batch_config, get_batch_config_arg
+from modules.shared.utils import exclude_tmp_paths, get_repo_root
 
 install()
 # -----------------------------------------------------------------------------/
@@ -17,4 +18,12 @@ install()
 print(f"Repository: '{get_repo_root()}'")
 
 resnet50_nocrop_fish_tester = ResNet50NoCropFishTester()
-resnet50_nocrop_fish_tester.run("4.test_by_fish.toml")
+args = get_batch_config_arg()
+
+if args.batch_mode == True:
+    
+    config_paths = sorted(exclude_tmp_paths(get_batch_config(__file__)))
+    for config_path in config_paths:
+        resnet50_nocrop_fish_tester.run(config_path)
+
+else: resnet50_nocrop_fish_tester.run("4.test_by_fish.toml")
